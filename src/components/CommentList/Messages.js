734cdf98228067c91a20506a14cb99c5a4a7463e
Neil Rotstan <neil@rotstan.org>
2018-02-05 07:35:24 -0800

    Show task links on comments in View Challenges.
    The comment list in the create-and-manage View Challenges page now
    includes links with each comment to the task the comment was created
    upon.

---
+++ src/components/CommentList/Messages.js
@@ -0,0 +1,11 @@
+import { defineMessages } from 'react-intl'
+
+/**
+ * Internationalized messages for use with CommentList
+ */
+export default defineMessages({
+  viewTaskLabel: {
+    id: "CommentList.controls.viewTask.label",
+    defaultMessage: "View Task"
+  },
+})
