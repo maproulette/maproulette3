b5d57c11f58ea23aba54b4d152d8554ab2934181
Neil Rotstan <neil@rotstan.org>
2018-02-06 13:23:41 -0800

    Compute missing task centerpoint. Fixes #6
    If the task being worked upon is missing a location, compute the
    center-point from the task's features if possible.

--- src/components/HOCs/WithTaskCenterPoint/WithTaskCenterPoint.js
+++ src/components/HOCs/WithTaskCenterPoint/WithTaskCenterPoint.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import center from '@turf/center'
 import _get from 'lodash/get'
+import _isObject from 'lodash/isObject'
 import { latLng } from 'leaflet'
 
 export default function(WrappedComponent) {
   class WithTaskCenterPoint extends Component {
     render() {
-      // The server gives us the center-point of a task as a standard GeoJSON
-      // Point, which is (Lng, Lat), but Leaflet maps wants (Lat, Lng).
-      // Note: not all tasks include a location, so be resilient.
-      const centerPoint = latLng(
-        _get(this.props, 'task.location.coordinates[1]', 0),
-        _get(this.props, 'task.location.coordinates[0]', 0)
-      )
+      let centerPoint = _get(this.props, 'task.location.coordinates')
+
+      // Not all tasks have a center-point. In that case, we try to calculate
+      // one ourselves based on the task features.
+      if (!centerPoint && _isObject(_get(this.props, 'task.geometries'))) {
+        centerPoint = _get(center(this.props.task.geometries), 'geometry.coordinates')
+      }
+
+      // If all our efforts failed, default to (0, 0).
+      if (!centerPoint) {
+        centerPoint = [0, 0]
+      }
+
+      // Our centerpoint is a standard GeoJSON Point, which is (Lng, Lat), but
+      // Leaflet maps want (Lat, Lng).
+      centerPoint = latLng(centerPoint[1], centerPoint[0])
 
       return <WrappedComponent centerPoint={centerPoint} {...this.props} />
     }
