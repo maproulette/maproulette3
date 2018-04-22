8feaaf46430434087bbce509ae584c602c15900c
Neil Rotstan <neil@rotstan.org>
2018-04-22 08:02:52 -0700

    Don't keep leaderboard in redux store.

--- src/components/HOCs/WithLeaderboard/WithLeaderboard.js
+++ src/components/HOCs/WithLeaderboard/WithLeaderboard.js
@@ -1,52 +1,72 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import _isObject from 'lodash/isObject'
+import _isArray from 'lodash/isArray'
+import _isBoolean from 'lodash/isBoolean'
+import _map from 'lodash/map'
 import { fetchLeaderboard }
        from '../../../services/Leaderboard/Leaderboard'
-import _get from 'lodash/get'
-import _omit from 'lodash/omit'
 
 /**
  * WithLeaderboard provides leaderboard and leaderboardLoading props containing
- * the current leaderboard data and loading status from the redux store.
+ * the current leaderboard data and loading status.
  *
  * @author [Neil Rotstan](https://github.com/nrotstan)
  */
-const WithLeaderboard = function(WrappedComponent) {
+const WithLeaderboard = function(WrappedComponent, initialStartDate) {
   return class extends Component {
     state = {
-      startDate: null,
+      startDate: initialStartDate,
+      leaderboard: null,
+      leaderboardLoading: false,
+    }
+
+    leaderboardParams = startDate => {
+      const params = new Map([['startDate', startDate],
+                              ['endDate', null],
+                              ['onlyEnabled', true],
+                              ['forProjects', null],
+                              ['forChallenges', null]])
+
+      if (_isObject(this.props.leaderboardOptions)) {
+        if (_isBoolean(this.props.leaderboardOptions.onlyEnabled)) {
+          params.set('onlyEnabled', this.props.leaderboardOptions.onlyEnabled)
+        }
+
+        if (this.props.leaderboardOptions.filterChallenges &&
+            _isArray(this.props.challenges)) {
+          params.set('forChallenges', [_map(this.props.challenges, 'id')])
+        }
+      }
+
+      return params.values()
+    }
+
+    updateLeaderboard = startDate => {
+      this.setState({leaderboardLoading: true})
+
+      fetchLeaderboard(...this.leaderboardParams(startDate)).then(leaderboard => {
+        this.setState({leaderboard, leaderboardLoading: false})
+      })
     }
 
     setLeaderboardStartDate = startDate => {
-      this.setState({startDate})
-    }
-
-    componentDidMount() {
-      this.props.fetchLeaderboard()
-    }
-
-    componentWillUpdate(nextProps, nextState) {
-      if (nextState.startDate !== this.state.startDate) {
-        this.props.fetchLeaderboard(nextState.startDate)
+      if (startDate !== this.state.startDate) {
+        this.setState({startDate})
+        this.updateLeaderboard(startDate)
       }
     }
 
+    componentDidMount() {
+      this.updateLeaderboard(this.state.startDate)
+    }
+
     render() {
-      return <WrappedComponent leaderboard={_get(this.props, 'leaderboard.leaderboard')}
-                               leaderboardLoading={_get(this.props, 'leaderboard.loading')}
+      return <WrappedComponent leaderboard={this.state.leaderboard}
+                               leaderboardLoading={this.state.leaderboardLoading}
                                setLeaderboardStartDate={this.setLeaderboardStartDate}
-                               {..._omit(this.props, ['leaderboard', 'fetchLeaderboard'])} />
+                               {...this.props} />
     }
   }
 }
   
-export const mapStateToProps = state => ({
-  leaderboard: state.currentLeaderboard,
-})
-
-export const mapDispatchToProps = dispatch => ({
-  fetchLeaderboard: (startDate=null) => dispatch(fetchLeaderboard(startDate))
-})
-
-export default WrappedComponent =>
-  connect(mapStateToProps, mapDispatchToProps)(WithLeaderboard(WrappedComponent))
+export default WithLeaderboard
