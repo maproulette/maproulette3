2a1bc043dba262b0446e513f069b3ea914a2bb58
Neil Rotstan <neil@rotstan.org>
2018-07-03 08:33:28 -0700

    Add 404 page.

---
+++ src/components/PageNotFound/Messages.js
@@ -0,0 +1,21 @@
+import { defineMessages } from 'react-intl'
+
+/**
+ * Internationalized messages for use with PageNotFound
+ */
+export default defineMessages({
+  missingPage: {
+    id: "PageNotFound.message",
+    defaultMessage: "Sorry, nothing here but open ocean.",
+  },
+
+  returnTo: {
+    id: "PageNotFound.returnTo",
+    defaultMessage: "Return to",
+  },
+
+  homePage: {
+    id: "PageNotFound.homePage",
+    defaultMessage: "home page",
+  },
+})
