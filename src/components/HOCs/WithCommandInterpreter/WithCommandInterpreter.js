324e54b9a5fea20bed6e3d43389837ecba0d291a
Kelli Rotstan <kelli@rotstan.org>
2018-11-26 12:21:12 -0800

    Minor changes as requested by code review.
    * Replace substr with substring.
    * Change executeCommand to use switch
    * Add unit test

--- src/components/HOCs/WithCommandInterpreter/WithCommandInterpreter.js
+++ src/components/HOCs/WithCommandInterpreter/WithCommandInterpreter.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-import _startsWith from 'lodash/startsWith'
 import _split from 'lodash/split'
 import _map from 'lodash/map'
 import _omit from 'lodash/omit'
@@ -22,66 +21,14 @@ const WithCommandInterpreter = function(WrappedComponent) {
       commandString: null,
     }
 
-    /**
-     * Executes the appropriate search type based on the start of
-     * the query string
-     */
-    executeCommand = commandString => {
-      if (_startsWith(commandString, 'm/') && commandString.length > 2) {
-        const query = commandString.substr(2)
-        let bounds = null
-
-        // If four points are given then we have a bounding box
-        if (_split(query, ',').length === 4) {
-          bounds = _map(_split(query, ','), (point) => parseFloat(point))
-
-        }
-        // If only two points are given then we have a center point
-        else if (_split(query, ',').length === 2) {
-          const centerpoint = _map(_split(query, ','), (point) => parseFloat(point))
-          bounds = this.determineBoundingBox(...centerpoint)
-        }
-
-        if (bounds) {
-          // We need to clear the search first so that any string searches won't
-          // be hanging around in redux
-          this.props.clearSearch()
-          this.props.setChallengeSearchMapBounds(bounds, 3, true)
-        }
-      }
-      else {
-        let query = commandString
-
-        if (_startsWith(commandString, 's/') && commandString.length > 2) {
-          query = commandString.substr(2)
-        }
-
-        // Standard search query
-        this.props.setSearch(query)
-      }
-
+    executeSearch = commandString => {
+      executeCommand(this.props, commandString)
       this.setState({commandString})
     }
 
     clearSearch = commandString => {
       this.props.clearSearch()
       this.setState({commandString: null})
-    }
-
-    /**
-    * Returns the bouding box from the given centerpoint coordinates
-    */
-    determineBoundingBox(centerpointLong, centerpointLat) {
-      const bboxWidth = parseFloat(process.env.REACT_APP_NEARBY_LONGITUDE_LENGTH)
-      const bboxHeight = parseFloat(process.env.REACT_APP_NEARBY_LATITUDE_LENGTH)
-
-      // Build WSEN bounds array
-      return ([
-        centerpointLong - (bboxWidth / 2.0),
-        centerpointLat - (bboxHeight / 2.0),
-        centerpointLong + (bboxWidth / 2.0),
-        centerpointLat + (bboxHeight / 2.0)
-      ])
     }
 
     render() {
@@ -92,6 +39,64 @@ const WithCommandInterpreter = function(WrappedComponent) {
                                clearSearch={this.clearSearch} />
     }
   }
+}
+
+/**
+ * Executes the appropriate search type based on the start of
+ * the query string
+ */
+export const executeCommand = (props, commandString) => {
+  const command = commandString.length > 2 ? commandString.substring(0, 2) : null
+  let query = commandString.substring(2)
+
+  switch(command) {
+    case 'm/':
+      let bounds = null
+
+      // If four points are given then we have a bounding box
+      if (_split(query, ',').length === 4) {
+        bounds = _map(_split(query, ','), (point) => parseFloat(point))
+
+      }
+      // If only two points are given then we have a center point
+      else if (_split(query, ',').length === 2) {
+        const centerpoint = _map(_split(query, ','), (point) => parseFloat(point))
+        bounds = determineBoundingBox(...centerpoint)
+      }
+
+      if (bounds) {
+        // We need to clear the search first so that any string searches won't
+        // be hanging around in redux
+        props.clearSearch()
+        props.setChallengeSearchMapBounds(bounds, 3, true)
+      }
+      break;
+    case 's/':
+    default:
+      if (command !== 's/') {
+        query = commandString
+      }
+
+      // Standard search query
+      props.setSearch(query)
+      break;
+  }
+}
+
+/**
+* Returns the bouding box from the given centerpoint coordinates
+*/
+const determineBoundingBox = (centerpointLong, centerpointLat) => {
+  const bboxWidth = parseFloat(process.env.REACT_APP_NEARBY_LONGITUDE_LENGTH)
+  const bboxHeight = parseFloat(process.env.REACT_APP_NEARBY_LATITUDE_LENGTH)
+
+  // Build WSEN bounds array
+  return ([
+    centerpointLong - (bboxWidth / 2.0),
+    centerpointLat - (bboxHeight / 2.0),
+    centerpointLong + (bboxWidth / 2.0),
+    centerpointLat + (bboxHeight / 2.0)
+  ])
 }
 
 WithCommandInterpreter.propTypes = {
