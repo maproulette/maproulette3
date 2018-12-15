d8030ad3be893f08a18d64c04ca7e4708c6a6360
Kelli Rotstan <kelli@rotstan.org>
2018-12-15 15:12:10 -0800

    Change admin search to show projects with most challenges first
    When doing an admin project search on both matching projects
    and challenges adds sort so that those projects with the
    most number of matching challenges are shown first.

--- src/components/HOCs/WithPagedProjects/WithPagedProjects.js
+++ src/components/HOCs/WithPagedProjects/WithPagedProjects.js
@@ -5,6 +5,8 @@ import _omit from 'lodash/omit'
 import _isEmpty from 'lodash/isEmpty'
 import _slice from 'lodash/slice'
 import _sortBy from 'lodash/sortBy'
+import _filter from 'lodash/filter'
+import _reverse from 'lodash/reverse'
 import { RESULTS_PER_PAGE } from '../../../services/Search/Search'
 
 export default function(WrappedComponent,
@@ -17,10 +19,20 @@ export default function(WrappedComponent,
       const numberResultsToShow = (currentPage + 1) * resultsPerPage
 
       let pagedProjects = this.props[projectsProp]
-      pagedProjects = _sortBy(pagedProjects, (p) => p.displayName)
 
       const hasMoreResults = (pagedProjects.length > numberResultsToShow) || this.props.isLoading
-      pagedProjects = _slice(pagedProjects, 0, numberResultsToShow)
+
+      // Only sort by display name if we do not have a challenge search going
+      if (!this.props.adminChallengesSearchActive) {
+        pagedProjects = _sortBy(pagedProjects, (p) => p.displayName.toLowerCase())
+        pagedProjects = _slice(pagedProjects, 0, numberResultsToShow)
+      }
+      // Otherwise sort by the number of matching challenges
+      else {
+        pagedProjects = _reverse(_sortBy(pagedProjects, (p) => {
+          return _filter(this.props.filteredChallenges, (c) => c.parent === p.id).length
+        }))
+      }
 
       if (_isEmpty(outputProp)) {
         outputProp = projectsProp
