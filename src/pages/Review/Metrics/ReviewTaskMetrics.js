ac6fe09be9248650d770a7a0b4744670336d3516
Ryan Scherler <ryan@eastslope.studio>
2019-05-01 15:00:09 -0700

    Eslint remove unused messages import.

--- src/pages/Review/Metrics/ReviewTaskMetrics.js
+++ src/pages/Review/Metrics/ReviewTaskMetrics.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import classNames from 'classnames'
-import messages from './Messages'
 import ChallengeProgress from '../../../components/ChallengeProgress/ChallengeProgress'
 
 
