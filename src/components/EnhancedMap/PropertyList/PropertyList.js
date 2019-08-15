6bfb1c5540cd694cdbf70b6fad2228e1e8a93bd8
Neil Rotstan <neil@rotstan.org>
2019-08-15 11:07:05 -0700

    Ignore object task properties on properties popup
    * When displaying a popup of a task's tags/properties, skip any that are
    structured as objects
    
    * Use internationalized messages in PropertyList component
    
    * Fix minor display issues when no properties are available

--- src/components/EnhancedMap/PropertyList/PropertyList.js
+++ src/components/EnhancedMap/PropertyList/PropertyList.js
@@ -1,6 +1,10 @@
 import React from 'react'
+import { FormattedMessage } from 'react-intl'
+import _compact from 'lodash/compact'
 import _map from 'lodash/map'
 import _isEmpty from 'lodash/isEmpty'
+import _isObject from 'lodash/isObject'
+import messages from './Messages'
 
 /**
  * Renders a list of properties in a table, intended for use in a map popup
@@ -11,10 +15,19 @@ const PropertyList = props => {
   const tagInfo = process.env.REACT_APP_TAGINFO_SERVER_URL
 
   if (_isEmpty(props.featureProperties)) {
-    return <div className="feature-properties empty">No Properties</div>
+    return (
+      <div className="feature-properties empty">
+        <h3>{props.header ? props.header : <FormattedMessage {...messages.title} />}</h3>
+        <span className="mr-ml-5"><FormattedMessage {...messages.noProperties} /></span>
+      </div>
+    )
   }
 
-  const rows = _map(props.featureProperties, (value, key) => {
+  const rows = _compact(_map(props.featureProperties, (value, key) => {
+    if (_isObject(value)) {
+      return null
+    }
+
     const link =
       !_isEmpty(tagInfo) ?
       <a target="_blank" rel="noopener noreferrer" href={`${tagInfo}/keys/${key}`}>{key}</a> :
@@ -26,11 +39,11 @@ const PropertyList = props => {
         <td className="value">{value}</td>
       </tr>
     )
-  })
+  }))
 
   return (
     <div className="feature-properties">
-      <h3>{props.header || "Properties"}</h3>
+      <h3>{props.header ? props.header : <FormattedMessage {...messages.title} />}</h3>
       <table className="property-list table">
         <tbody>{rows}</tbody>
       </table>
