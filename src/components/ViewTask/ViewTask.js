0e4137eeeb9aa86bd88574f2dae90108e02b3dba
Neil Rotstan <neil@rotstan.org>
2019-10-04 15:54:07 -0700

    Don't syntax-highlight missing task geometry
    * When viewing the GeoJSON for a task in the Create & Manage area, wait
    for the task geometry to be fully loaded before trying to display
    syntax-highlighted JSON

--- src/components/ViewTask/ViewTask.js
+++ src/components/ViewTask/ViewTask.js
@@ -4,12 +4,13 @@ import { Light as SyntaxHighlighter } from 'react-syntax-highlighter'
 import jsonLang from 'react-syntax-highlighter/dist/languages/hljs/json'
 import highlightColors from 'react-syntax-highlighter/dist/styles/hljs/github'
 import BusySpinner from '../BusySpinner/BusySpinner'
+import _get from 'lodash/get'
 
 SyntaxHighlighter.registerLanguage('json', jsonLang);
 
 export default class ViewTask extends Component {
   render() {
-    if (!this.props.task) {
+    if (!_get(this.props, 'task.geometries')) {
       return <BusySpinner />
     }
 
