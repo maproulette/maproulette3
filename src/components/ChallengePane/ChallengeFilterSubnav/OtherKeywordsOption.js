82714a0f4f79c676784c5e56f99e7cbdb7a64f82
Neil Rotstan <neil@rotstan.org>
2019-10-25 08:06:26 -0700

    Fix "other" keyword filter styling. Fixes #960
    * Fix styling of "Other" option in keyword-filter dropdown menu,
    including making text and submission button colors easier to see
    
    * Fix keyword-filter dropdown so that it closes properly when an "other"
    keyword is submitted or cleared

--- src/components/ChallengePane/ChallengeFilterSubnav/OtherKeywordsOption.js
+++ src/components/ChallengePane/ChallengeFilterSubnav/OtherKeywordsOption.js
@@ -6,7 +6,7 @@ import messages from './Messages'
 export default class OtherKeywordsOption extends Component {
   render() {
     return (
-      <div className="mr-flex mr-items-center">
+      <div className="mr-flex mr-items-center mr-py-3">
         <label
           className="mr-text-green-lighter mr-mr-4 mr-cursor-pointer"
         >
@@ -17,7 +17,9 @@ export default class OtherKeywordsOption extends Component {
           suppressIcon
           showDoneButton
           leftAligned
-          className="mr-appearance-none mr-outline-none mr-rounded-none mr-bg-transparent mr-text-white mr-border-b mr-border-green-lighter"
+          className="mr-appearance-none mr-outline-none mr-rounded-none mr-bg-transparent mr-text-white mr-border-b mr-border-green-lighter mr-h-6 mr-pb-1"
+          inputClassName="mr-text-white"
+          buttonClassName="mr-fill-green-lighter"
         />
       </div>
     )
