cb2e6cffa6554471796d2c7589f2ee1cd7d1ba5d
Kelli Rotstan <kelli@rotstan.org>
2019-11-18 14:02:56 -0800

    Add toggle to minimize 'zoom in to view tasks' message box
    Add toggle that will minimize zoom in message box to hide
    extra navigation controls. Also, minimize this message box
    when user manually zooms in.

--- src/components/TaskClusterMap/ZoomInMessage.js
+++ src/components/TaskClusterMap/ZoomInMessage.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import { FormattedMessage } from 'react-intl'
 import SvgSymbol from '../SvgSymbol/SvgSymbol'
+import _isUndefined from 'lodash/isUndefined'
 import LocationSearchBox from '../EnhancedMap/SearchControl/LocationSearchBox'
 import { ChallengeLocation}
        from '../../services/Challenge/ChallengeLocation/ChallengeLocation'
 import { toLatLngBounds } from '../../services/MapBounds/MapBounds'
+import { MIN_ZOOM } from './TaskClusterMap'
 import messages from './Messages'
 
 /**
@@ -16,24 +18,38 @@ import messages from './Messages'
  */
 export class ZoomInMessage extends Component {
   state = {
-    minimized: false
+  }
+
+  componentDidUpdate(prevProps) {
+    // If user has zoomed in then we want to minimized
+    if (prevProps.zoom < this.props.zoom &&
+        prevProps.zoom <= MIN_ZOOM && !this.state.minimized) {
+      this.setState({minimized: true})
+    }
+    // If user has zoomed out then we want to un-minimize
+    else if (prevProps.zoom > this.props.zoom &&
+             this.props.zoom <= MIN_ZOOM && this.state.minimized) {
+      this.setState({minimized: false})
+    }
+    // If minimized is undefined this is the first time this is loading
+    else if (_isUndefined(this.state.minimized)) {
+      this.setState({minimized: (this.props.zoom > MIN_ZOOM)})
+    }
   }
 
   render() {
-
     return (
       <div className="mr-absolute mr-pin-t mr-mt-3 mr-w-full mr-flex mr-justify-center">
         <div className="mr-z-5 mr-flex-col mr-items-center mr-bg-black-40 mr-text-white mr-rounded">
           <div className="mr-py-2 mr-px-3 mr-text-center">
             <FormattedMessage {...messages.zoomInForTasksLabel} />
-            <div className="mr-pl-4 mr-inline-block">
-              <a onClick={() => this.setState({minimized: !this.state.minimized})}>
-                <SvgSymbol
-                  sym="icon-cheveron-down"
-                  viewBox="0 0 20 20"
-                  className="mr-fill-current mr-w-5 mr-h-5 mr-text-white"
-                />
-              </a>
+            <div className="mr-pl-8 mr-inline-block"
+                 onClick={() => this.setState({minimized: !this.state.minimized})}>
+              <SvgSymbol
+                sym="icon-cheveron-down"
+                viewBox="0 0 20 20"
+                className="mr-fill-current mr-w-5 mr-h-5 mr-text-white"
+              />
             </div>
           </div>
           {!this.state.minimized &&
