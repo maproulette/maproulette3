b91a00bccb2cf8cfe8f6d989ea4dc09937681fc6
Neil Rotstan <neil@rotstan.org>
2020-01-29 17:02:54 -0800

    Support arbitrary styling of task features
    * Allow task features to be styled with specified simplestyle values in
    addition to (or instead of) simplestyle properties found explicitly on
    the feature itself

--- src/interactions/TaskFeature/AsSimpleStyleableFeature.js
+++ src/interactions/TaskFeature/AsSimpleStyleableFeature.js
@@ -3,11 +3,29 @@ import 'leaflet-vectoricon'
 import { getType } from '@turf/invariant'
 import _isUndefined from 'lodash/isUndefined'
 import _each from 'lodash/each'
+import _map from 'lodash/map'
+import _compact from 'lodash/compact'
+import _fromPairs from 'lodash/fromPairs'
+import _intersection from 'lodash/intersection'
 import _keys from 'lodash/keys'
+import _pick from 'lodash/pick'
+import _merge from 'lodash/merge'
 import { colors } from '../../tailwind'
 
 
-// Maps simplestyle property names to corresponding leaflet style option names
+const supportedSimplestylePointProperties = [
+  'marker-color', 'marker-size',
+]
+
+const supportedSimplestyleLineProperties = [
+  'stroke', 'stroke-width', 'stroke-opacity', 'fill', 'fill-opacity',
+]
+
+// Names of supported simplestyle properties
+export const supportedSimplestyles =
+  supportedSimplestyleLineProperties.concat(supportedSimplestylePointProperties)
+
+// Maps simplestyle line properties to corresponding leaflet style options
 const simplestyleLineToLeafletMapping = {
   'stroke': 'color',
   'stroke-width': 'weight',
@@ -15,14 +33,6 @@ const simplestyleLineToLeafletMapping = {
   'fill': 'fillColor',
   'fill-opacity': 'fillOpacity',
 }
-
-const simplestylePointProperties = [
-  'marker-color', 'marker-size',
-]
-
-// Names of supported simplestyle properties
-export const supportedSimplestyles =
-  _keys(simplestyleLineToLeafletMapping).concat(simplestylePointProperties)
 
 /**
  * AsSimpleStylableFeature adds functionality for interpreting
@@ -39,28 +49,54 @@ export class AsSimpleStyleableFeature {
    */
   styleLeafletLayer(layer) {
     if (this.properties) {
-      if (getType(layer.feature) === 'Point') {
-        this.styleLeafletMarkerLayer(layer)
-      }
-      else {
-        this.styleLeafletPathLayer(layer)
-      }
+      this.styleLeafletLayerWithStyles(layer, this.simplestyleFeatureProperties(), false)
     }
   }
 
-  styleLeafletPathLayer(layer) {
+  /**
+   * Styles the given Leaflet layer using the given simplestyles object of
+   * simplestyle property names and desired values. If
+   * mergeWithFeatureProperties is true (the default) then all simplestyle
+   * properties specified on the feature will also be used, but overriden by
+   * given styles where duplicated
+   */
+  styleLeafletLayerWithStyles(layer, simplestyles, mergeWithFeatureProperties=true) {
+    const styles =
+      mergeWithFeatureProperties ?
+      _merge({}, this.simplestyleFeatureProperties(), simplestyles) :
+      simplestyles
+
+    if (getType(layer.feature) === 'Point') {
+      const supportedStyles =
+        _intersection(_keys(styles), supportedSimplestylePointProperties)
+
+      this.styleLeafletMarkerLayer(layer, _pick(styles, supportedStyles))
+    }
+    else {
+      const supportedStyles =
+        _intersection(_keys(styles), supportedSimplestyleLineProperties)
+
+      this.styleLeafletPathLayer(layer, _pick(styles, supportedStyles))
+    }
+  }
+
+  /**
+   * Styles a leaflet path layer with the given line-compatible simplestyles
+   */
+  styleLeafletPathLayer(layer, lineStyles) {
     if (!layer.setStyle) {
       return
     }
 
-    _each(simplestyleLineToLeafletMapping, (leafletStyle, simplestyleProperty) => {
-      if (!_isUndefined(this.properties[simplestyleProperty])) {
-        layer.setStyle({[leafletStyle]: this.properties[simplestyleProperty]})
-      }
+    _each(lineStyles, (styleValue, styleName) => {
+      layer.setStyle({[simplestyleLineToLeafletMapping[styleName]]: styleValue})
     })
   }
 
-  styleLeafletMarkerLayer(layer) {
+  /**
+   * Styles a leaflet marker layer with the given point-compatible simplestyles
+   */
+  styleLeafletMarkerLayer(layer, pointStyles) {
     if (!layer.setIcon) {
       return
     }
@@ -84,37 +120,47 @@ export class AsSimpleStyleableFeature {
 
     let useCustomMarker = false
 
-    _each(simplestylePointProperties, (simplestyleProperty) => {
-      if (!_isUndefined(this.properties[simplestyleProperty])) {
-        switch (simplestyleProperty) {
-          case 'marker-color':
-            useCustomMarker = true
-            customMarker.style.fill = this.properties[simplestyleProperty]
-            break
-          case 'marker-size':
-            useCustomMarker = true
-            switch (this.properties[simplestyleProperty]) {
-              case 'small':
-                customMarker.svgHeight = 20
-                customMarker.svgWidth = 20
-                break
-              case 'large':
-                customMarker.svgHeight = 60
-                customMarker.svgWidth = 60
-                break
-              default:
-                break
-            }
-            break
-          default:
-            break
-        }
+    _each(pointStyles, (styleValue, styleName) => {
+      switch (styleName) {
+        case 'marker-color':
+          useCustomMarker = true
+          customMarker.style.fill = styleValue
+          break
+        case 'marker-size':
+          useCustomMarker = true
+          switch (styleValue) {
+            case 'small':
+              customMarker.svgHeight = 20
+              customMarker.svgWidth = 20
+              break
+            case 'large':
+              customMarker.svgHeight = 60
+              customMarker.svgWidth = 60
+              break
+            default:
+              // medium is already the default size
+              break
+          }
+          break
+        default:
+          break
       }
     })
 
     if (useCustomMarker) {
       layer.setIcon(L.vectorIcon(customMarker))
     }
+  }
+
+  /**
+   * Retrieves all simplestyle properties specified on this feature
+   */
+  simplestyleFeatureProperties() {
+    return _fromPairs(_compact(_map(supportedSimplestyles, simplestyleProperty => (
+      !_isUndefined(this.properties[simplestyleProperty]) ?
+      [simplestyleProperty, this.properties[simplestyleProperty]] :
+      null
+    ))))
   }
 }
 
