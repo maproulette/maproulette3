3d4f2ecb2d0f2d28992aaa746bc4c7b4daac4776
Kelli Rotstan <kelli@rotstan.org>
2020-05-27 14:54:48 -0700

    Add support for a priority 'location rule'
    * Add priority rule for a 'location rule' that allows for
      a bounding box to be given to determine if task location
      is inside or outside bounds. Bounds can be chosen from
      a map.

---
+++ src/components/AreaSelect/AreaSelect.js
@@ -0,0 +1,40 @@
+import L from 'leaflet'
+import './leaflet-areaselect'
+import { injectIntl } from 'react-intl'
+import { MapControl, withLeaflet } from 'react-leaflet'
+import './AreaSelect.scss'
+
+/**
+ * Leaflet AreaSelect that allows selection of a bounding box.
+ *
+ * Note: An object passed to the constructor will be available as `this.options`
+ *
+ * Code provided by: https://github.com/heyman/leaflet-areaselect/
+ */
+const AreaSelectLeaflet = L.Control.extend({
+  onAdd: function(map) {
+    const areaSelect = L.areaSelect({width:200, height:300});
+    areaSelect.addTo(map)
+
+    // Send a callback when the bounds change
+    areaSelect.on("change", () => {
+      this.options.onBoundsChanged(areaSelect.getBounds())
+    })
+
+    return L.DomUtil.create('div')
+  }})
+
+
+/**
+ * AreaSelect is a react-leaflet component intended to be
+ * used as a child of a react-leaflet Map instance, such as EnhancedMap. When
+ * clicked, the control activates
+ */
+export class AreaSelect extends MapControl {
+  // props will be available as `options` field in the leaflet control
+  createLeafletElement(props) {
+    return new AreaSelectLeaflet(props)
+  }
+}
+
+export default withLeaflet(injectIntl(AreaSelect))
---
+++ src/components/BoundsSelectorModal/BoundsSelectorModal.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react'
+import { FormattedMessage } from 'react-intl'
+import bbox from '@turf/bbox'
+import _split from 'lodash/split'
+import classNames from 'classnames'
+import { Map, ZoomControl } from 'react-leaflet'
+import { toLatLngBounds, fromLatLngBounds }
+       from '../../services/MapBounds/MapBounds'
+import SourcedTileLayer
+       from '../EnhancedMap/SourcedTileLayer/SourcedTileLayer'
+import Modal from '../Modal/Modal'
+import External from '../External/External'
+import MapPane from '../EnhancedMap/MapPane/MapPane'
+import AreaSelect from '../AreaSelect/AreaSelect'
+import { defaultLayerSource }
+       from '../../services/VisibleLayer/LayerSources'
+import messages from './Messages'
+import './BoundsSelectorMap.scss'
+
+/**
+ * BoundsSelectorModal presents a modal that displays a
+ * map on which a bounding box can be selected.
+ *
+ * @author [Kelli Rotstan](https://github.com/krotstan)
+ */
+export default class BoundsSelectorModal extends Component {
+  state = {
+    active: false,
+    lat: 0,
+    lng: 0,
+  }
+
+  dismiss = (defaultBounds) => {
+    this.setState({active: false})
+
+    if (this.state.mapBounds || defaultBounds) {
+      const bbox = fromLatLngBounds(this.state.mapBounds || defaultBounds)
+
+      if (bbox) {
+        this.props.onChange(bbox.join(','))
+      }
+    }
+  }
+
+  componentDidUpdate(prevPrps, prevState) {
+
+  }
+  render() {
+    const boundingBox = this.props.value ?
+      toLatLngBounds(_split(this.props.value, ',')) :
+        this.props.bounding ? toLatLngBounds(bbox(this.props.bounding)) : null
+
+    return (
+      <React.Fragment>
+        <button className="mr-button mr-button mr-button--small mr-ml-4"
+          onClick={(e) => {
+            console.log(e)
+            console.log("Button click.")
+            e.stopPropagation()
+            e.preventDefault()
+            this.setState({active: true})
+          }}>
+          {this.props.buttonName}
+        </button>
+
+        {this.state.active &&
+          <External>
+            <Modal isActive wide onClose={() => this.dismiss()}>
+              <div className="mr-overflow-y-auto mr-max-h-screen80">
+                <div className="mr-bg-blue-dark mr-p-8 mr-text-white mr-text-center">
+                  <div>
+                    <h2 className="mr-text-yellow mr-text-2xl mr-mb-2">
+                      <FormattedMessage {...messages.header} />
+                    </h2>
+                    <div className="form mr-mt-2 mr-py-2">
+                      <p className="mr-mr-4 mr-text-md">
+                        <FormattedMessage {...messages.primaryMessage} />
+                      </p>
+                    </div>
+
+                  </div>
+                  <div className={classNames("boundsSelector-map", this.props.className)}>
+                    <MapPane>
+                      <Map bounds={boundingBox}
+                           zoomControl={false}>
+                        <ZoomControl className="mr-z-1000" position='topright' />
+                        <SourcedTileLayer source={defaultLayerSource()} skipAttribution={true} />
+                        <AreaSelect onBoundsChanged={(mapBounds) => this.setState({mapBounds})} />
+                      </Map>
+                    </MapPane>
+                  </div>
+                  <div className="mr-text-center mr-mt-6">
+                    <button className="mr-button" onClick={() => this.dismiss(boundingBox)}>
+                      <FormattedMessage {...messages.dismiss} />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </Modal>
+          </External>
+        }
+      </React.Fragment>
+    )
+  }
+}
