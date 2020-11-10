5186483bbb898bb74d4114923847e2a62061116a
Neil Rotstan <neil@rotstan.org>
2020-11-10 07:29:35 -0800

    Show lat/lon instead of lon/lat in location widget

---
+++ src/components/TaskPane/TaskLatLon/Messages.js
@@ -0,0 +1,16 @@
+import { defineMessages } from 'react-intl'
+
+/**
+ * Internationalized messages for use with TaskLatLon
+ */
+export default defineMessages({
+  latLonLabel: {
+    id: 'TaskLatLon.latLon.label',
+    defaultMessage: "Lat/Lon:",
+  },
+
+  latLonValue: {
+    id: 'TaskLatLon.latLon.value',
+    defaultMessage: "{lat}, {lon}",
+  },
+})
