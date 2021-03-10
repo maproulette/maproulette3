aa90bb987209b746c3bd3f849ef02197cef4d0a2
jschwarz2030 <79289630+jschwarz2030@users.noreply.github.com>
2021-03-10 07:16:36 -0600

    Import/Export modal design updates (#1582)
    * set up a test
    
    * update import/export layout design
    
    * fix export names
    
    * replace teams home page svg
    
    * remove failing test

--- src/components/ImportFileModal/ImportFileModal.js
+++ src/components/ImportFileModal/ImportFileModal.js
@@ -1,95 +1,104 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import Dropzone from 'react-dropzone'
-import { FormattedMessage } from 'react-intl'
-import External from '../External/External'
-import Modal from '../Modal/Modal'
-import BusySpinner from '../BusySpinner/BusySpinner'
-import SvgSymbol from '../SvgSymbol/SvgSymbol'
-import messages from './Messages'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Dropzone from "react-dropzone";
+import { FormattedMessage } from "react-intl";
+import External from "../External/External";
+import Modal from "../Modal/Modal";
+import BusySpinner from "../BusySpinner/BusySpinner";
+import SvgSymbol from "../SvgSymbol/SvgSymbol";
+import messages from "./Messages";
 
 export default class ImportLayoutModal extends Component {
   state = {
     importingFile: false,
-  }
+  };
 
   render() {
     return (
       <External>
-        <Modal isActive onClose={this.props.onCancel}>
-          <div className="mr-flex mr-justify-between">
-            <div className="mr-pt-12">
+        <Modal narrow isActive onClose={this.props.onCancel}>
+          <div>
+            <div className="mr-flex mr-justify-center mr-my-8">
               <SvgSymbol
-                sym="illustration-tasks"
-                viewBox="0 0 200 171"
-                className="mr-h-40 mr-mr-12"
+                sym="cat-and-laptop"
+                style={{
+                  height: 131,
+                  width: 185,
+                }}
               />
             </div>
             <div className="mr-w-full">
               <h2 className="mr-text-white mr-text-4xl mr-mb-4">
                 {this.props.header}
               </h2>
-
               <div className="mr-mt-2">
-                {this.state.importingFiles ? <BusySpinner /> :
-                 <div>
-                   <Dropzone
-                     acceptClassName="active"
-                     multiple={false}
-                     disablePreview
-                     onDrop={files => {
-                       this.setState({importingFiles: true})
-                       this.props.onUpload(files[0]).then(() => {
-                         this.setState({importingFiles: false})
-                         this.props.onCancel()
-                       }).catch(() => {
-                        this.setState({importingFiles: false})
-                       })
-                     }}
-                   >
-                     {({acceptedFiles, getRootProps, getInputProps}) => {
-                       const body = acceptedFiles.length > 0 ? <p>{acceptedFiles[0].name}</p> : (
-                         <span className="mr-flex mr-items-center">
-                           <SvgSymbol
-                             viewBox='0 0 20 20'
-                             sym="upload-icon"
-                             className="mr-fill-current mr-w-3 mr-h-3 mr-mr-4"
-                           />
-                           <FormattedMessage {...messages.uploadLabel} />
-                           <input {...getInputProps()} />
-                         </span>
-                       )
+                {this.state.importingFiles ? (
+                  <BusySpinner />
+                ) : (
+                  <div>
+                    <Dropzone
+                      acceptClassName="active"
+                      multiple={false}
+                      disablePreview
+                      onDrop={(files) => {
+                        this.setState({ importingFiles: true });
+                        this.props
+                          .onUpload(files[0])
+                          .then(() => {
+                            this.setState({ importingFiles: false });
+                            this.props.onCancel();
+                          })
+                          .catch(() => {
+                            this.setState({ importingFiles: false });
+                          });
+                      }}
+                    >
+                      {({ acceptedFiles, getRootProps, getInputProps }) => {
+                        const body =
+                          acceptedFiles.length > 0 ? (
+                            <p>{acceptedFiles[0].name}</p>
+                          ) : (
+                            <span className="mr-flex mr-items-center">
+                              <SvgSymbol
+                                viewBox="0 0 20 20"
+                                sym="upload-icon"
+                                className="mr-fill-current mr-w-3 mr-h-3 mr-mr-4"
+                              />
+                              <FormattedMessage {...messages.uploadLabel} />
+                              <input {...getInputProps()} />
+                            </span>
+                          );
 
-                       return (
-                         <div
-                           className="dropzone mr-text-green-lighter mr-border-green-lighter mr-border-2 mr-rounded mr-p-4 mr-mx-auto mr-mt-12 mr-cursor-pointer"
-                           {...getRootProps()}
-                         >
-                           {body}
-                         </div>
-                       )
-                     }}
-                   </Dropzone>
-                   <div className="mr-flex mr-justify-end mr-items-center mr-mt-8">
-                     <button
-                       className="mr-button mr-button--white"
-                       onClick={this.props.onCancel}
-                     >
-                       <FormattedMessage {...messages.cancelLabel} />
-                     </button>
+                        return (
+                          <div
+                            className="dropzone mr-text-green-lighter mr-border-green-lighter mr-border-2 mr-rounded mr-p-4 mr-mx-auto mr-mt-6 mr-cursor-pointer"
+                            {...getRootProps()}
+                          >
+                            {body}
+                          </div>
+                        );
+                      }}
+                    </Dropzone>
+                    <div className="mr-flex mr-justify-end mr-items-center mr-mt-8">
+                      <button
+                        className="mr-button mr-button--white"
+                        onClick={this.props.onCancel}
+                      >
+                        <FormattedMessage {...messages.cancelLabel} />
+                      </button>
+                    </div>
                   </div>
-                </div>
-                }
+                )}
               </div>
             </div>
           </div>
         </Modal>
       </External>
-    )
+    );
   }
 }
 
 ImportLayoutModal.propTypes = {
   onCancel: PropTypes.func.isRequired,
   onUpload: PropTypes.func.isRequired,
-}
+};
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,37 +1,36 @@
-import "@testing-library/jest-dom"
-import React from 'react';
-import Enzyme, { shallow, render, mount } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+import "@testing-library/jest-dom";
+import React, { Fragment } from "react";
+import Enzyme, { shallow, render, mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 import { render as rtlRender } from "@testing-library/react";
-import { Provider } from 'react-redux'
-import { IntlProvider } from 'react-intl'
-import { initializeStore } from './PersistedStore'
+import { Provider } from "react-redux";
+import { IntlProvider } from "react-intl";
+import { initializeStore } from "./PersistedStore";
 
 // React 16 Enzyme adapter
-Enzyme.configure({ adapter: new Adapter() })
+Enzyme.configure({ adapter: new Adapter() });
 
 // Make Enzyme functions available in all test files without importing
-global.shallow = shallow
-global.render = render
-global.mount = mount
+global.shallow = shallow;
+global.render = render;
+global.mount = mount;
 
 // React testing library methods
-const reduxStore = initializeStore()
+const reduxStore = initializeStore();
 
 global.withProvider = (
-    ui,
-    {
-      initialState,
-      store = reduxStore,
-      ...renderOptions
-    } = {}
-  ) => {
-    function Wrapper({ children }) {
-      return (
+  ui,
+  { initialState, store = reduxStore, ...renderOptions } = {}
+) => {
+  function Wrapper({ children }) {
+    return (
+      <Fragment>
         <Provider store={store}>
           <IntlProvider locale="en">{children}</IntlProvider>
         </Provider>
-      )
-    }
-    return rtlRender(ui, { wrapper: Wrapper, ...renderOptions })
-  } 
+        <div id="external-root"></div>
+      </Fragment>
+    );
+  }
+  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
+};
