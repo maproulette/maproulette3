6713e0c8ed8b913fac71e3df1ef27e0dbbcb64ad
Jimmy Schwarzenberger <jschwarz2030@gmail.com>
2021-04-27 16:17:59 -0500

    add email requirement notice and disable controls

--- src/components/AdminPane/AdminPane.js
+++ src/components/AdminPane/AdminPane.js
@@ -1,25 +1,24 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { Switch, Route, withRouter } from 'react-router-dom'
-import MediaQuery from 'react-responsive'
-import AsManager from '../../interactions/User/AsManager'
-import SignIn from '../../pages/SignIn/SignIn'
-import WithStatus from '../HOCs/WithStatus/WithStatus'
-import WithCurrentUser from '../HOCs/WithCurrentUser/WithCurrentUser'
-import ScreenTooNarrow
-       from '../ScreenTooNarrow/ScreenTooNarrow'
-import EditChallenge from './Manage/ManageChallenges/EditChallenge/EditChallenge'
-import EditProject from './Manage/EditProject/EditProject'
-import ManageChallengeList from './Manage/VirtualProjects/ManageChallengeList'
-import EditTask from './Manage/ManageTasks/EditTask/EditTask'
-import InspectTask from './Manage/InspectTask/InspectTask'
-import ProjectsDashboard from './Manage/ProjectsDashboard/ProjectsDashboard'
-import ProjectDashboard from './Manage/ProjectDashboard/ProjectDashboard'
-import ChallengeDashboard from './Manage/ChallengeDashboard/ChallengeDashboard'
-import BusySpinner from '../BusySpinner/BusySpinner'
-import './Manage/Widgets/widget_registry.js'
-import './AdminPane.scss'
-
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Switch, Route, withRouter } from "react-router-dom";
+import MediaQuery from "react-responsive";
+import AsManager from "../../interactions/User/AsManager";
+import SignIn from "../../pages/SignIn/SignIn";
+import WithStatus from "../HOCs/WithStatus/WithStatus";
+import WithCurrentUser from "../HOCs/WithCurrentUser/WithCurrentUser";
+import ScreenTooNarrow from "../ScreenTooNarrow/ScreenTooNarrow";
+import EditChallenge from "./Manage/ManageChallenges/EditChallenge/EditChallenge";
+import EditProject from "./Manage/EditProject/EditProject";
+import ManageChallengeList from "./Manage/VirtualProjects/ManageChallengeList";
+import EditTask from "./Manage/ManageTasks/EditTask/EditTask";
+import InspectTask from "./Manage/InspectTask/InspectTask";
+import ProjectsDashboard from "./Manage/ProjectsDashboard/ProjectsDashboard";
+import ProjectDashboard from "./Manage/ProjectDashboard/ProjectDashboard";
+import ChallengeDashboard from "./Manage/ChallengeDashboard/ChallengeDashboard";
+import BusySpinner from "../BusySpinner/BusySpinner";
+import EmailRequirementNotice from "./Manage/EmailRequirementNotice/EmailRequirementNotice";
+import "./Manage/Widgets/widget_registry.js";
+import "./AdminPane.scss";
 
 /**
  * AdminPane is the top-level component for administration functions. It has a
@@ -31,56 +30,77 @@ import './AdminPane.scss'
  */
 export class AdminPane extends Component {
   componentDidUpdate(prevProps) {
-    if (this.props.location.pathname !== prevProps.location.pathname &&
-        this.props.location.search !== prevProps.location.search) {
-      window.scrollTo(0, 0)
+    if (
+      this.props.location.pathname !== prevProps.location.pathname &&
+      this.props.location.search !== prevProps.location.search
+    ) {
+      window.scrollTo(0, 0);
     }
   }
 
   render() {
     // The user needs to be logged in.
-    const manager = AsManager(this.props.user)
+    const manager = AsManager(this.props.user);
     if (!manager.isLoggedIn()) {
-      return (
-        this.props.checkingLoginStatus ?
+      return this.props.checkingLoginStatus ? (
         <div className="admin mr-flex mr-justify-center mr-py-8 mr-w-full mr-bg-blue">
           <BusySpinner />
-        </div> :
+        </div>
+      ) : (
         <SignIn {...this.props} />
-      )
+      );
     }
 
     return (
       <React.Fragment>
+        <EmailRequirementNotice />
         <div className="admin mr-bg-gradient-r-green-dark-blue mr-text-white">
           <div className="admin-pane">
             <Switch>
-              <Route exact path='/admin/project/:projectId/challenge/:challengeId'
-                      component={ChallengeDashboard} />
               <Route
                 exact
-                path={[
-                  '/admin/projects/new',
-                  '/admin/project/:projectId/edit',
-                ]}
+                path="/admin/project/:projectId/challenge/:challengeId"
+                component={ChallengeDashboard}
+              />
+              <Route
+                exact
+                path={["/admin/projects/new", "/admin/project/:projectId/edit"]}
                 component={EditProject}
               />
               <Route
                 exact
                 path={[
-                  '/admin/project/:projectId/challenges/new',
-                  '/admin/project/:projectId/challenge/:challengeId/edit',
-                  '/admin/project/:projectId/challenge/:challengeId/clone',
+                  "/admin/project/:projectId/challenges/new",
+                  "/admin/project/:projectId/challenge/:challengeId/edit",
+                  "/admin/project/:projectId/challenge/:challengeId/clone",
                 ]}
                 component={EditChallenge}
               />
-              <Route exact path='/admin/project/:projectId/challenge/:challengeId/task/:taskId/edit'
-                      component={EditTask} />
-              <Route exact path='/admin/project/:projectId/challenge/:challengeId/task/:taskId/inspect'
-                      component={InspectTask} />
-              <Route exact path='/admin/virtual/project/:projectId/challenges/manage' component={ManageChallengeList} />
-              <Route exact path='/admin/projects' component={ProjectsDashboard} />
-              <Route exact path='/admin/project/:projectId' component={ProjectDashboard} />
+              <Route
+                exact
+                path="/admin/project/:projectId/challenge/:challengeId/task/:taskId/edit"
+                component={EditTask}
+              />
+              <Route
+                exact
+                path="/admin/project/:projectId/challenge/:challengeId/task/:taskId/inspect"
+                component={InspectTask}
+              />
+              <Route
+                exact
+                path="/admin/virtual/project/:projectId/challenges/manage"
+                component={ManageChallengeList}
+              />
+              <Route
+                exact
+                path="/admin/projects"
+                component={ProjectsDashboard}
+              />
+              <Route
+                exact
+                path="/admin/project/:projectId"
+                component={ProjectDashboard}
+              />
               <Route component={ProjectsDashboard} />
             </Switch>
           </div>
@@ -88,14 +108,14 @@ export class AdminPane extends Component {
         <MediaQuery query="(max-width: 1023px)">
           <ScreenTooNarrow />
         </MediaQuery>
-    </React.Fragment>
-    )
+      </React.Fragment>
+    );
   }
 }
 
 AdminPane.propTypes = {
   /** router location */
   location: PropTypes.object.isRequired,
-}
+};
 
-export default WithStatus(WithCurrentUser(withRouter(AdminPane)))
+export default WithStatus(WithCurrentUser(withRouter(AdminPane)));
--- src/components/AdminPane/Manage/ChallengeCard/ChallengeControls.js
+++ src/components/AdminPane/Manage/ChallengeCard/ChallengeControls.js
@@ -1,159 +1,196 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import classNames from 'classnames'
-import { FormattedMessage } from 'react-intl'
-import { Link } from 'react-router-dom'
-import { CopyToClipboard } from 'react-copy-to-clipboard'
-import _get from 'lodash/get'
-import _isObject from 'lodash/isObject'
-import _isFinite from 'lodash/isFinite'
-import _merge from 'lodash/merge'
-import AsManager from '../../../../interactions/User/AsManager'
-import AsManageableChallenge
-       from '../../../../interactions/Challenge/AsManageableChallenge'
-import { ChallengeStatus, isUsableChallengeStatus }
-       from  '../../../../services/Challenge/ChallengeStatus/ChallengeStatus'
-import RebuildTasksControl from '../RebuildTasksControl/RebuildTasksControl'
-import ProjectPickerModal from '../ProjectPickerModal/ProjectPickerModal'
-import ConfirmAction from '../../../ConfirmAction/ConfirmAction'
-import messages from '../ChallengeDashboard/Messages'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import { FormattedMessage } from "react-intl";
+import { Link } from "react-router-dom";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import _get from "lodash/get";
+import _isObject from "lodash/isObject";
+import _isFinite from "lodash/isFinite";
+import _merge from "lodash/merge";
+import AsManager from "../../../../interactions/User/AsManager";
+import AsManageableChallenge from "../../../../interactions/Challenge/AsManageableChallenge";
+import {
+  ChallengeStatus,
+  isUsableChallengeStatus,
+} from "../../../../services/Challenge/ChallengeStatus/ChallengeStatus";
+import RebuildTasksControl from "../RebuildTasksControl/RebuildTasksControl";
+import ProjectPickerModal from "../ProjectPickerModal/ProjectPickerModal";
+import ConfirmAction from "../../../ConfirmAction/ConfirmAction";
+import messages from "../ChallengeDashboard/Messages";
+
+const isEmailRequired = (user) => {
+  if (process.env.REACT_APP_EMAIL_ENFORCEMENT === "required") {
+    if (!user?.settings?.email) {
+      return true;
+    }
+  }
+
+  return false;
+};
 
 export default class ChallengeControls extends Component {
   state = {
     pickingProject: false,
-  }
+  };
 
   projectPickerCanceled = () => {
-    this.setState({pickingProject: false})
-    this.props.onControlComplete && this.props.onControlComplete()
-  }
+    this.setState({ pickingProject: false });
+    this.props.onControlComplete && this.props.onControlComplete();
+  };
 
-  moveToProject = project => {
-    this.setState({pickingProject: false})
-    this.props.moveChallenge(this.props.challenge.id, project.id)
-    this.props.onControlComplete && this.props.onControlComplete()
-  }
+  moveToProject = (project) => {
+    this.setState({ pickingProject: false });
+    this.props.moveChallenge(this.props.challenge.id, project.id);
+    this.props.onControlComplete && this.props.onControlComplete();
+  };
 
-  deleteChallenge = parent => {
-    this.props.deleteChallenge(parent.id, this.props.challenge.id)
-  }
+  deleteChallenge = (parent) => {
+    this.props.deleteChallenge(parent.id, this.props.challenge.id);
+  };
 
   render() {
     if (!this.props.challenge) {
-      return null
+      return null;
     }
 
-    let parent = null
+    let parent = null;
     if (_isObject(this.props.challenge.parent)) {
-      parent = this.props.challenge.parent
-    }
-    else if (_isFinite(this.props.challenge.parent) &&
-             this.props.challenge.parent === _get(this.props, 'project.id')) {
-      parent = this.props.project
+      parent = this.props.challenge.parent;
+    } else if (
+      _isFinite(this.props.challenge.parent) &&
+      this.props.challenge.parent === _get(this.props, "project.id")
+    ) {
+      parent = this.props.project;
     }
 
-    const inVirtualProject = _get(this.props, 'project.isVirtual', false)
-    const manager = AsManager(this.props.user)
-    const projectId = _get(this.props.challenge, 'parent.id', this.props.challenge.parent)
-    const status = _get(this.props.challenge, 'status', ChallengeStatus.none)
-    const hasTasks = _get(this.props.challenge, 'actions.total', 0) > 0
+    const inVirtualProject = _get(this.props, "project.isVirtual", false);
+    const manager = AsManager(this.props.user);
+    const projectId = _get(
+      this.props.challenge,
+      "parent.id",
+      this.props.challenge.parent
+    );
+    const status = _get(this.props.challenge, "status", ChallengeStatus.none);
+    const hasTasks = _get(this.props.challenge, "actions.total", 0) > 0;
+    const requiresEmail = isEmailRequired(this.props.user);
+
+    if (requiresEmail) {
+      return (
+        <div className="mr-text-red-light">
+          Challenge controls are disabled until an email is provided.
+        </div>
+      );
+    }
 
     return (
       <div className={this.props.className}>
-        {hasTasks && isUsableChallengeStatus(status, true) &&
-         status !== ChallengeStatus.finished &&
-          <Link
-            to={`/challenge/${this.props.challenge.id}`}
-            className={this.props.controlClassName}
-          >
-            <FormattedMessage {...messages.startChallengeLabel} />
-          </Link>
-        }
-        {this.props.includeCopyURL &&
+        {hasTasks &&
+          isUsableChallengeStatus(status, true) &&
+          status !== ChallengeStatus.finished && (
+            <Link
+              to={`/challenge/${this.props.challenge.id}`}
+              className={this.props.controlClassName}
+            >
+              <FormattedMessage {...messages.startChallengeLabel} />
+            </Link>
+          )}
+        {this.props.includeCopyURL && (
           <CopyToClipboard
             text={`${process.env.REACT_APP_URL}/browse/challenges/${this.props.challenge.id}`}
-            onCopy={this.props.onControlComplete}>
+            onCopy={this.props.onControlComplete}
+          >
             <div
-              className={classNames(this.props.controlClassName,
-                "mr-text-green-lighter hover:mr-text-white mr-cursor-pointer")}>
+              className={classNames(
+                this.props.controlClassName,
+                "mr-text-green-lighter hover:mr-text-white mr-cursor-pointer"
+              )}
+            >
               <FormattedMessage {...messages.copyChallengeURLLabel} />
             </div>
           </CopyToClipboard>
-        }
-        {!inVirtualProject && manager.canWriteProject(parent) &&
+        )}
+        {!inVirtualProject && manager.canWriteProject(parent) && (
           <React.Fragment>
             <Link
               to={{
-                pathname:`/admin/project/${projectId}/` +
+                pathname:
+                  `/admin/project/${projectId}/` +
                   `challenge/${this.props.challenge.id}/edit`,
-                state: _get(this.props.searchCriteria, 'filters')
+                state: _get(this.props.searchCriteria, "filters"),
               }}
               className={this.props.controlClassName}
             >
-              <FormattedMessage {...messages.editChallengeLabel } />
+              <FormattedMessage {...messages.editChallengeLabel} />
             </Link>
 
-            {manager.canAdministrateProject(parent) &&
-             // eslint-disable-next-line jsx-a11y/anchor-is-valid
-             <a
-               onClick={() => this.setState({pickingProject: true})}
-               className={this.props.controlClassName}
-             >
-               <FormattedMessage {...messages.moveChallengeLabel} />
-             </a>
-            }
+            {manager.canAdministrateProject(parent) && (
+              // eslint-disable-next-line jsx-a11y/anchor-is-valid
+              <a
+                onClick={() => this.setState({ pickingProject: true })}
+                className={this.props.controlClassName}
+              >
+                <FormattedMessage {...messages.moveChallengeLabel} />
+              </a>
+            )}
 
             {this.props.onChallengeDashboard &&
-             AsManageableChallenge(this.props.challenge).isRebuildable() &&
-              <RebuildTasksControl {...this.props} />
-            }
+              AsManageableChallenge(this.props.challenge).isRebuildable() && (
+                <RebuildTasksControl {...this.props} />
+              )}
 
             <Link
               to={{
-                pathname: `/admin/project/${projectId}/` +
-                          `challenge/${this.props.challenge.id}/clone`,
-                state: _merge({cloneChallenge: true},
-                   _get(this.props.searchCriteria, 'filters'))
+                pathname:
+                  `/admin/project/${projectId}/` +
+                  `challenge/${this.props.challenge.id}/clone`,
+                state: _merge(
+                  { cloneChallenge: true },
+                  _get(this.props.searchCriteria, "filters")
+                ),
               }}
               className={this.props.controlClassName}
             >
-              <FormattedMessage {...messages.cloneChallengeLabel } />
+              <FormattedMessage {...messages.cloneChallengeLabel} />
             </Link>
 
-            {manager.canAdministrateProject(parent) &&
-             <ConfirmAction
-              title={this.props.intl.formatMessage(messages.deleteChallengeConfirm)}
-              prompt={
-                <React.Fragment>
-                  <div className="mr-text-mango mr-mb-6 mr-text-lg">
-                    {this.props.challenge.name}
-                  </div>
-                  <div>
-                    <FormattedMessage {...messages.deleteChallengeWarn} />
-                  </div>
-                </React.Fragment>
-              }>
-               {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-               <a
-                 onClick={() => this.deleteChallenge(parent)}
-                 className={this.props.controlClassName}
-               >
-                 <FormattedMessage {...messages.deleteChallengeLabel } />
-               </a>
-             </ConfirmAction>
-            }
-            {this.state.pickingProject &&
-             <ProjectPickerModal
+            {manager.canAdministrateProject(parent) && (
+              <ConfirmAction
+                title={this.props.intl.formatMessage(
+                  messages.deleteChallengeConfirm
+                )}
+                prompt={
+                  <React.Fragment>
+                    <div className="mr-text-mango mr-mb-6 mr-text-lg">
+                      {this.props.challenge.name}
+                    </div>
+                    <div>
+                      <FormattedMessage {...messages.deleteChallengeWarn} />
+                    </div>
+                  </React.Fragment>
+                }
+              >
+                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                <a
+                  onClick={() => this.deleteChallenge(parent)}
+                  className={this.props.controlClassName}
+                >
+                  <FormattedMessage {...messages.deleteChallengeLabel} />
+                </a>
+              </ConfirmAction>
+            )}
+            {this.state.pickingProject && (
+              <ProjectPickerModal
                 {...this.props}
                 currentProjectId={projectId}
                 onCancel={this.projectPickerCanceled}
                 onSelectProject={this.moveToProject}
               />
-            }
+            )}
           </React.Fragment>
-        }
+        )}
       </div>
-    )
+    );
   }
 }
 
@@ -168,4 +205,4 @@ ChallengeControls.propTypes = {
   onControlComplete: PropTypes.func,
   /** Some controls are only available from the challenge dashboard */
   onChallengeDashboard: PropTypes.bool,
-}
+};
