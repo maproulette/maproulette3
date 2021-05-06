ae8a4279f8dfe06748afbdcfb2b0df4713e536eb
jschwarz2030 <79289630+jschwarz2030@users.noreply.github.com>
2021-05-06 11:38:48 -0500

    add Polish to language support (#1603)

--- src/services/User/Locale/Locale.js
+++ src/services/User/Locale/Locale.js
@@ -1,8 +1,8 @@
-import '@formatjs/intl-relativetimeformat/polyfill'
-import _map from 'lodash/map'
-import _isString from 'lodash/isString'
-import _fromPairs from 'lodash/fromPairs'
-import messages from './Messages'
+import "@formatjs/intl-relativetimeformat/polyfill";
+import _map from "lodash/map";
+import _isString from "lodash/isString";
+import _fromPairs from "lodash/fromPairs";
+import messages from "./Messages";
 
 // To add support for a new locale, add it to both `Locale` and `LocaleImports`
 // in this file, and then add a description of the new locale to the
@@ -10,116 +10,137 @@ import messages from './Messages'
 
 // Supported locales
 export const Locale = Object.freeze({
-  enUS: 'en-US',
-  es: 'es',
-  fr: 'fr',
-  de: 'de',
-  af: 'af',
-  ja: 'ja',
-  ko: 'ko',
-  nl: 'nl',
-  'pt-BR': 'pt-BR',
-  'cs-CZ': 'cs-CZ',
-  'fa-IR': 'fa-IR',
-  'ru-RU': 'ru-RU',
-  uk: 'uk',
-  vi: 'vi',
-  tr: 'tr',
-})
+  enUS: "en-US",
+  es: "es",
+  fr: "fr",
+  de: "de",
+  af: "af",
+  ja: "ja",
+  ko: "ko",
+  nl: "nl",
+  "pt-BR": "pt-BR",
+  "cs-CZ": "cs-CZ",
+  "fa-IR": "fa-IR",
+  "ru-RU": "ru-RU",
+  uk: "uk",
+  vi: "vi",
+  tr: "tr",
+  pl: "pl",
+});
 
 // Dynamic imports to load locale data and translation files
 const LocaleImports = {
-  [Locale.enUS]: () => Promise.all([
-    import('../../../lang/en-US.json'),
-    import('@formatjs/intl-relativetimeformat/locale-data/en'),
-  ]),
-  [Locale.es]: () => Promise.all([
-    import('../../../lang/es.json'),
-    import('@formatjs/intl-relativetimeformat/locale-data/es'),
-  ]),
-  [Locale.fr]: () => Promise.all([
-    import('../../../lang/fr.json'),
-    import('@formatjs/intl-relativetimeformat/locale-data/fr'),
-  ]),
-  [Locale.de]: () => Promise.all([
-    import('../../../lang/de.json'),
-    import('@formatjs/intl-relativetimeformat/locale-data/de'),
-  ]),
-  [Locale.af]: () => Promise.all([
-    import('../../../lang/af.json'),
-    import('@formatjs/intl-relativetimeformat/locale-data/af'),
-  ]),
-  [Locale.ja]: () => Promise.all([
-    import('../../../lang/ja.json'),
-    import('@formatjs/intl-relativetimeformat/locale-data/ja'),
-  ]),
-  [Locale.ko]: () => Promise.all([
-    import('../../../lang/ko.json'),
-    import('@formatjs/intl-relativetimeformat/locale-data/ko'),
-  ]),
-  [Locale.nl]: () => Promise.all([
-    import('../../../lang/nl.json'),
-    import('@formatjs/intl-relativetimeformat/locale-data/nl'),
-  ]),
-  [Locale.uk]: () => Promise.all([
-    import('../../../lang/uk.json'),
-    import('@formatjs/intl-relativetimeformat/locale-data/uk'),
-  ]),
-  [Locale.vi]: () => Promise.all([
-    import('../../../lang/vi.json'),
-    import('@formatjs/intl-relativetimeformat/locale-data/vi'),
-  ]),
-  [Locale["pt-BR"]]: () => Promise.all([
-    import('../../../lang/pt_BR.json'),
-    import('@formatjs/intl-relativetimeformat/locale-data/pt'),
-  ]),
-  [Locale["cs-CZ"]]: () => Promise.all([
-    import('../../../lang/cs_CZ.json'),
-    import('@formatjs/intl-relativetimeformat/locale-data/cs'),
-  ]),
-  [Locale["fa-IR"]]: () => Promise.all([
-    import('../../../lang/fa_IR.json'),
-    import('@formatjs/intl-relativetimeformat/locale-data/fa'),
-  ]),
-  [Locale["ru-RU"]]: () => Promise.all([
-    import('../../../lang/ru_RU.json'),
-    import('@formatjs/intl-relativetimeformat/locale-data/ru'),
-  ]),
-  [Locale.tr]: () => Promise.all([
-    import('../../../lang/tr.json'),
-    import('@formatjs/intl-relativetimeformat/locale-data/tr'),
-  ]),
-}
+  [Locale.enUS]: () =>
+    Promise.all([
+      import("../../../lang/en-US.json"),
+      import("@formatjs/intl-relativetimeformat/locale-data/en"),
+    ]),
+  [Locale.es]: () =>
+    Promise.all([
+      import("../../../lang/es.json"),
+      import("@formatjs/intl-relativetimeformat/locale-data/es"),
+    ]),
+  [Locale.fr]: () =>
+    Promise.all([
+      import("../../../lang/fr.json"),
+      import("@formatjs/intl-relativetimeformat/locale-data/fr"),
+    ]),
+  [Locale.de]: () =>
+    Promise.all([
+      import("../../../lang/de.json"),
+      import("@formatjs/intl-relativetimeformat/locale-data/de"),
+    ]),
+  [Locale.af]: () =>
+    Promise.all([
+      import("../../../lang/af.json"),
+      import("@formatjs/intl-relativetimeformat/locale-data/af"),
+    ]),
+  [Locale.ja]: () =>
+    Promise.all([
+      import("../../../lang/ja.json"),
+      import("@formatjs/intl-relativetimeformat/locale-data/ja"),
+    ]),
+  [Locale.ko]: () =>
+    Promise.all([
+      import("../../../lang/ko.json"),
+      import("@formatjs/intl-relativetimeformat/locale-data/ko"),
+    ]),
+  [Locale.nl]: () =>
+    Promise.all([
+      import("../../../lang/nl.json"),
+      import("@formatjs/intl-relativetimeformat/locale-data/nl"),
+    ]),
+  [Locale.uk]: () =>
+    Promise.all([
+      import("../../../lang/uk.json"),
+      import("@formatjs/intl-relativetimeformat/locale-data/uk"),
+    ]),
+  [Locale.vi]: () =>
+    Promise.all([
+      import("../../../lang/vi.json"),
+      import("@formatjs/intl-relativetimeformat/locale-data/vi"),
+    ]),
+  [Locale["pt-BR"]]: () =>
+    Promise.all([
+      import("../../../lang/pt_BR.json"),
+      import("@formatjs/intl-relativetimeformat/locale-data/pt"),
+    ]),
+  [Locale["cs-CZ"]]: () =>
+    Promise.all([
+      import("../../../lang/cs_CZ.json"),
+      import("@formatjs/intl-relativetimeformat/locale-data/cs"),
+    ]),
+  [Locale["fa-IR"]]: () =>
+    Promise.all([
+      import("../../../lang/fa_IR.json"),
+      import("@formatjs/intl-relativetimeformat/locale-data/fa"),
+    ]),
+  [Locale["ru-RU"]]: () =>
+    Promise.all([
+      import("../../../lang/ru_RU.json"),
+      import("@formatjs/intl-relativetimeformat/locale-data/ru"),
+    ]),
+  [Locale.tr]: () =>
+    Promise.all([
+      import("../../../lang/tr.json"),
+      import("@formatjs/intl-relativetimeformat/locale-data/tr"),
+    ]),
+  [Locale.pl]: () =>
+    Promise.all([
+      import("../../../lang/pl.json"),
+      import("@formatjs/intl-relativetimeformat/locale-data/pl"),
+    ]),
+};
 
 /**
  * Returns true if the given locale matches a supported locale, false
  * otherwise.
  */
-export const isSupportedLocale = function(locale) {
-  return _isString(locale) && _isString(Locale[locale])
-}
+export const isSupportedLocale = function (locale) {
+  return _isString(locale) && _isString(Locale[locale]);
+};
 
 /**
  * Dynamically load and return the translated messages for the given locale
  */
-export const loadTranslatedMessages = async function(locale) {
+export const loadTranslatedMessages = async function (locale) {
   if (!isSupportedLocale(locale)) {
-    locale = defaultLocale()
+    locale = defaultLocale();
   }
 
-  const [messages] = await LocaleImports[locale]()
-  return messages
-}
+  const [messages] = await LocaleImports[locale]();
+  return messages;
+};
 
 /**
  * Returns the default locale configured in the .env file, or U.S.  English if
  * no default is configured or if the configured locale isn't supported.
  */
-export const defaultLocale = function() {
-  const configured = process.env.REACT_APP_DEFAULT_LOCALE
+export const defaultLocale = function () {
+  const configured = process.env.REACT_APP_DEFAULT_LOCALE;
 
-  return isSupportedLocale(configured) ? configured : Locale.enUS
-}
+  return isSupportedLocale(configured) ? configured : Locale.enUS;
+};
 
 /**
  * Returns an object mapping label messages for locale values to raw
@@ -132,9 +153,10 @@ export const defaultLocale = function() {
  */
 export const labelMessagesByLocale = _fromPairs(
   _map(messages, (message, key) => [Locale[key], message])
-)
+);
 
 /** Returns object containing localized labels  */
-export const localeLabels = intl => _fromPairs(
-  _map(messages, (message, key) => [key, intl.formatMessage(message)])
-)
+export const localeLabels = (intl) =>
+  _fromPairs(
+    _map(messages, (message, key) => [key, intl.formatMessage(message)])
+  );
