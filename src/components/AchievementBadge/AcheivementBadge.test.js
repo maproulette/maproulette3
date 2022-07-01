a308fc420e9715fda3c83e81b01a5edb6764ec01
Collin Beczak <88843144+CollinBeczak@users.noreply.github.com>
2022-07-01 15:24:34 -0500

    Update AcheivementBadge.test.js

--- src/components/AchievementBadge/AcheivementBadge.test.js
+++ src/components/AchievementBadge/AcheivementBadge.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import AchievementBadge from "./AchievementBadge";
 
 describe("AchievementBadge", () => {
-  it("renders with achievement 15", async () => {
+  it("renders with achievement 16", async () => {
     await global.withProvider(
       <AchievementBadge achievement={16} stackDepth={3} />
     );
