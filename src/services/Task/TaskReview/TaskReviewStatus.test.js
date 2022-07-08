30ab16d429338d9f26fa4e071e7b711eeb9a6ba4
Collin Beczak <88843144+CollinBeczak@users.noreply.github.com>
2022-07-08 17:15:02 -0500

    Add Unit Tests (#1780)

---
+++ src/services/Task/TaskReview/TaskReviewStatus.test.js
@@ -0,0 +1,29 @@
+import {
+  isMetaReviewStatus,
+  isNeeded,
+  hasBeenReviewed,
+  isNeedsReviewStatus,
+} from "./TaskReviewStatus";
+
+describe("isMetaReviewStatus", () => {
+  it("returns true if the function isMetaReviewStatus doesnt have a parameter", () => {
+    const value = isMetaReviewStatus();
+
+    expect(value).toBe(true);
+  });
+  it("returns false if the function isNeedsReviewStatus doesnt have a parameter", () => {
+    const value = isNeedsReviewStatus();
+
+    expect(value).toBe(false);
+  });
+  it("returns false if the function isNeeded doesnt have a parameter", () => {
+    const value = isNeeded();
+
+    expect(value).toBe(false);
+  });
+  it("returns true if the function hasBeenReviewed doesnt have a parameter", () => {
+    const value = hasBeenReviewed();
+
+    expect(value).toBe(true);
+  });
+});
---
+++ src/services/Widget/ChallengeFilter/ChallengeFilter.test.js
@@ -0,0 +1,22 @@
+import { messagesByFilter } from "./ChallengeFilter";
+
+describe("messagesByFilter", () => {
+  it("returns specific object when ran", () => {
+    const value = messagesByFilter;
+
+    expect(value).toStrictEqual({
+      archived: {
+        defaultMessage: "Archived",
+        id: "Dashboard.ChallengeFilter.archived.label",
+      },
+      pinned: {
+        defaultMessage: "Pinned",
+        id: "Dashboard.ChallengeFilter.pinned.label",
+      },
+      visible: {
+        defaultMessage: "Discoverable",
+        id: "Dashboard.ChallengeFilter.visible.label",
+      },
+    });
+  });
+});
