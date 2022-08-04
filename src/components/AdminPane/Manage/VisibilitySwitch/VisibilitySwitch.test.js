048960f85057011995fc686a14088850ef1f392a
Jimmy <jschwarz2030@gmail.com>
2022-08-03 21:02:19 -0500

    add tests for new components

--- src/components/AdminPane/Manage/VisibilitySwitch/VisibilitySwitch.test.js
+++ src/components/AdminPane/Manage/VisibilitySwitch/VisibilitySwitch.test.js
@@ -1,13 +1,27 @@
 import "@testing-library/jest-dom";
 import * as React from "react";
+import { fireEvent } from "@testing-library/react";
 import { VisibilitySwitch } from "./VisibilitySwitch";
 
 describe("Visibility Switch", () => {
   it("renders Visibility Switch if a challenge object is provided", () => {
     const { container } = global.withProvider(
-      <VisibilitySwitch updateEnabled={() => null} challenge={{ enabled: false }} />
+      <VisibilitySwitch updateEnabled={() => updateEnabled()} challenge={{ enabled: false }} />
     );
 
-    expect(container.firstChild.classList["0"]).toBe("mr-flex")
+    expect(container.firstChild.classList["0"]).toBe("mr-flex");
+  });
+
+  it("input onClick event does not call updateEnabled", () => {
+    const updateEnabled = jest.fn();
+    const { container } = global.withProvider(
+      <VisibilitySwitch updateEnabled={() => updateEnabled()} challenge={{ enabled: false }} />
+    );
+
+    const element = container.querySelector('input[type=checkbox]');
+
+    fireEvent.click(element);
+
+    expect(updateEnabled).toHaveBeenCalledTimes(0);
   });
 });
