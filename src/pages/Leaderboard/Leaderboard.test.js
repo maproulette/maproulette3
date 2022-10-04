a527ec499f9c36d36eecf9181820bec41dbb12ea
Jackson Tran <javtran@ucsc.edu>
2022-10-04 16:51:23 -0700

    clean up test

--- src/pages/Leaderboard/Leaderboard.test.js
+++ src/pages/Leaderboard/Leaderboard.test.js
@@ -1,7 +1,7 @@
 import React, { Component }  from 'react';
 import { shallow } from 'enzyme';
 import CardLeaderboard from './CardLeaderboard'
-import { fireEvent, screen } from '@testing-library/react';
+import RowLeaderboard from './RowLeaderboard'
 
 describe("hoverUser", () => {
     let wrapper;
@@ -15,6 +15,33 @@ describe("hoverUser", () => {
             score: 3,
         }
         wrapper = shallow(<CardLeaderboard leader={user}/>)
+    })
+
+    test('isHover is false on load', () => {
+        expect(wrapper.state().isHover).toEqual(false)
+    })
+
+    test('isHover is true on hover & false on leave', () => {
+        wrapper.instance().onHover()
+        expect(wrapper.state().isHover).toEqual(true)
+
+        wrapper.instance().onLeave()
+        expect(wrapper.state().isHover).toEqual(false)
+    })
+}) 
+
+describe("hoverUser", () => {
+    let wrapper;
+    
+    beforeEach(() => {
+        const user = {
+            id: 123,
+            name: 'test',
+            topChallenges: [],
+            rank: 1,
+            score: 3,
+        }
+        wrapper = shallow(<RowLeaderboard leader={user}/>)
     })
 
     test('isHover is false on load', () => {
