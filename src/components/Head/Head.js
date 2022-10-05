856f30a04d91b936c237282199276d400469f2b7
Yan Sun <tsun812@gmail.com>
2022-10-05 05:44:18 -0700

    refactor and bugfix Head.js

--- src/components/Head/Head.js
+++ src/components/Head/Head.js
@@ -3,10 +3,8 @@ import { useState, useEffect } from 'react';
 import { Helmet } from 'react-helmet';
 import { withRouter } from 'react-router';
 import WithCurrentUser from '../HOCs/WithCurrentUser/WithCurrentUser';
-import WithProject from '../HOCs/WithProject/WithProject'
-import WithChallenges from '../HOCs/WithChallenges/WithChallenges';
-import WithChallenge from '../HOCs/WithChallenge/WithChallenge';
-import WithBrowsedChallenge from '../HOCs/WithBrowsedChallenge/WithBrowsedChallenge';
+import WithCurrentProject from '../AdminPane/HOCs/WithCurrentProject/WithCurrentProject';
+import WithCurrentChallenge from '../AdminPane/HOCs/WithCurrentChallenge/WithCurrentChallenge';
 import { injectIntl } from 'react-intl';
 import _get from 'lodash/get'
 import _isEmpty from 'lodash/isEmpty'
@@ -17,10 +15,10 @@ const HeadTitle = (props) => {
 
   useEffect(() => {
     getPath(props.match.path)
-  }, [props.location.pathname, props.project, props.browsedChallenge])
+  }, [props.location.pathname, props.project, props.challenge, props.user])
 
   const findCurrentChallengeName = () => {
-    return props.browsedChallenge && props.browsedChallenge.name
+    return props.challenge && props.challenge.name
   }
 
   const findCurrentProjectName = () => {
@@ -28,11 +26,19 @@ const HeadTitle = (props) => {
   }
 
   const findCurrentUserName = () => {
-    return props.osmProfile && props.osmProfile.displayName
+    return props.user && props.user.osmProfile.displayName
   }
 
   const findCurrentCountryCode = () => {
     return _get(props, 'match.params.countryCode')
+  }
+
+  const findCurrentTaskId = () => {
+    return _get(props, 'match.params.taskId')
+  }
+
+  const findReviewType = () => {
+    return _get(props, 'match.params.showType')
   }
 
   const getPath = (path) => {
@@ -55,6 +61,12 @@ const HeadTitle = (props) => {
       else if (params === ':userId') {
         return findCurrentUserName()
       }
+      else if (params === ':taskId') {
+        return findCurrentTaskId()
+      }
+      else if (params === ':showType') {
+        return findReviewType()
+      }
       else {
         return params
       }
@@ -62,7 +74,7 @@ const HeadTitle = (props) => {
     let newTitle = _isEmpty(pathArr) ? process.env.REACT_APP_TITLE : process.env.REACT_APP_TITLE + '-' + pathArr.join('-')
     setTitle(newTitle)
   }
-
+  console.log(props)
   return (
     <>
       <Helmet>
@@ -72,9 +84,8 @@ const HeadTitle = (props) => {
   )
 }
 
-export default withRouter(WithCurrentUser(WithProject(
-  WithChallenges(
-    WithChallenge(
-      WithBrowsedChallenge(injectIntl(HeadTitle))
-    )
-  ))))
+export default withRouter(WithCurrentUser(WithCurrentProject(
+  WithCurrentChallenge(
+    injectIntl(HeadTitle)))
+)
+)
