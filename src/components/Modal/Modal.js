f59a14206f241701ac2a15fc32620edc63506a7d
jschwarz2030 <79289630+jschwarz2030@users.noreply.github.com>
2022-11-08 18:28:32 -0600

    maximize size of next nearby task map (#1854)

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -22,14 +22,16 @@ class Modal extends Component {
         />
         <div
           className={classNames(
-            "mr-z-250 mr-fixed mr-top-0 mr-left-0 mr-max-w-screen80", {
+            "mr-z-250 mr-fixed mr-top-0 mr-left-0", {
+            "mr-max-w-screen80": !this.props.fullScreen,
+            "mr-w-full mr-h-full": this.props.fullScreen,
             "md:mr-w-4/5 md:mr-top-5 md:mr-left-16": this.props.extraWide,
             "md:mr-w-2/3 md:mr-top-5 md:mr-left-16": this.props.wide,
             "md:mr-min-w-1/3 md:mr-w-1/3 md:mr-top-5 md:mr-left-33": this.props.narrow,
             "mr-w-full md:mr-w-1/4 md:mr-top-5 md:mr-left-37": this.props.extraNarrow,
             "md:mr-min-w-2/5 md:mr-w-2/5 md:mr-top-15 md:mr-left-30": this.props.medium,
             "md:mr-min-w-screen50 lg:mr-max-w-screen60 mr-w-full lg:mr-top-50 lg:mr-left-50 lg:mr--translate-1/2":
-              !this.props.extraWide && !this.props.wide && !this.props.narrow &&
+              !this.props.extraWide && !this.props.wide && !this.props.narrow && !this.props.fullScreen &&
               !this.props.extraNarrow && !this.props.medium
           })}
         >
@@ -37,8 +39,9 @@ class Modal extends Component {
             className={classNames(
               {'mr-p-8': !this.props.fullBleed,
                'mr-overflow-y-auto': !this.props.allowOverflow,
+               'mr-h-full': this.props.fullScreen,
                'mr-overflow-visible': this.props.allowOverflow},
-              'mr-relative mr-bg-blue-dark mr-rounded mr-shadow mr-w-full mr-w-full mr-w-md mr-mx-auto mr-max-h-screen90 mr-min-w-72',
+              'mr-relative mr-bg-blue-dark mr-rounded mr-shadow mr-w-full mr-w-md mr-mx-auto mr-max-h-screen90 mr-min-w-72',
               this.props.contentClassName
             )}
           >
