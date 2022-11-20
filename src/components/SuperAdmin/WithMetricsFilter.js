48c5584c897ac7d6b20f2d8d23a617560a024287
Yan Sun <tsun812@gmail.com>
2022-11-20 14:57:50 -0800

    fix project toggle by visible bug

--- src/components/SuperAdmin/WithMetricsFilter.js
+++ src/components/SuperAdmin/WithMetricsFilter.js
@@ -28,14 +28,19 @@ const WithMetricsFilter = function(WrappedComponent) {
           search: searchquery
         })  
       }
-
-      let challenges = entityFilters.visible ? this.props.challenges.filter(c => c.enabled) : this.props.challenges
-      challenges = entityFilters.archived ? challenges.filter(c => !c.isArchived) : challenges
-
-      let projects = entityFilters.visible? this.props.projects.filter(p => p.enabled): this.props.projects
-      projects = entityFilters.archived? projects.filter(p => !p.isArchived): projects
-      projects = entityFilters.virtual? projects.filter(p => p.isVirtual): projects
-
+      let challenges = this.props.challenges
+      let projects = this.props.projects
+      if (tab === 'challenges') {
+        console.log('here challenges')
+        challenges = entityFilters.visible ? this.props.challenges.filter(c => c.enabled) : this.props.challenges
+        challenges = entityFilters.archived ? challenges.filter(c => !c.isArchived) : challenges
+      }
+      else if (tab === 'projects') {
+        console.log('here projects')
+        projects = entityFilters.visible ? this.props.projects.filter(p => p.enabled) : this.props.projects
+        projects = entityFilters.archived ? projects.filter(p => !p.isArchived) : projects
+        projects = entityFilters.virtual ? projects.filter(p => p.isVirtual) : projects
+      }
       return (
         <WrappedComponent {...this.props} 
           challenges = {challenges} 
