852ea2822f29390911eb1227ebad9e590c61b1ac
Yan Sun <tsun812@gmail.com>
2022-12-13 11:43:44 -0800

    filter by dates and toggle together

--- src/components/SuperAdmin/WithMetricsFilter.js
+++ src/components/SuperAdmin/WithMetricsFilter.js
@@ -14,17 +14,19 @@ const WithMetricsFilter = function(WrappedComponent) {
         visible: params['hideUndiscoverable'] === 'true',
         archived: params['hideArchived'] === 'true',
         virtual: params['virtual'] === 'true',
-        from : params['from'],
-        to: params['to']
+        from : params['from'] || undefined,
+        to: params['to'] || undefined
       }
 
       const toggleFilter = (filterName) => {
         entityFilters[filterName] = !entityFilters[filterName]
         let searchquery = `?`
         searchquery += `tab=${tab}&`
-        searchquery += `hideUndiscoverable=${entityFilters.visible}&`
-        searchquery += `hideArchived=${entityFilters.archived}&`
-        searchquery += `virtual=${entityFilters.virtual}`
+        searchquery += tab !== 'users' && `&hideUndiscoverable=${entityFilters.visible}`
+        searchquery += tab !== 'users' && `&hideArchived=${entityFilters.archived}`
+        searchquery += tab === 'projects' && `&virtual=${entityFilters.virtual}`
+        searchquery += entityFilters.from ? `&from=${entityFilters.from}` : ''
+        searchquery += entityFilters.to ? `&to=${entityFilters.to}` : ''
         this.props.history.push({
           pathname: '/superadmin',
           search: searchquery
@@ -62,9 +64,8 @@ const WithMetricsFilter = function(WrappedComponent) {
       let users = this.props.users
       let startDate = new Date (entityFilters.from)
       let endDate = new Date(entityFilters.to)
-
       if (tab === 'challenges') {
-        challenges = entityFilters.visible ? this.props.challenges.filter(c => c.enabled) : this.props.challenges
+        challenges = entityFilters.visible ? challenges.filter(c => c.enabled) : challenges
         challenges = entityFilters.archived ? challenges.filter(c => !c.isArchived) : challenges
         challenges = entityFilters.from ? challenges.filter(c => {
           const date = new Date(c.created)
@@ -76,7 +77,7 @@ const WithMetricsFilter = function(WrappedComponent) {
         }) : challenges
       }
       else if (tab === 'projects') {
-        projects = entityFilters.visible ? this.props.projects.filter(p => p.enabled) : this.props.projects
+        projects = entityFilters.visible ? projects.filter(p => p.enabled) : projects
         projects = entityFilters.archived ? projects.filter(p => !p.isArchived) : projects
         projects = entityFilters.virtual ? projects.filter(p => p.isVirtual) : projects
         projects = entityFilters.from ? projects.filter(p => {
