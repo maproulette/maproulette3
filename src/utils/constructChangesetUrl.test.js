341e7b64f8a86801fe5296766a29939ad7165988
jschwarz2030 <79289630+jschwarz2030@users.noreply.github.com>
2023-01-04 14:22:03 -0500

    remove changesetUrl configs and make mandatory (#1909)

--- src/utils/constructChangesetUrl.test.js
+++ src/utils/constructChangesetUrl.test.js
@@ -16,19 +16,24 @@ describe("constructChangesetUrl", () => {
     expect(constructChangesetUrl()).toBe("");
   });
 
-  it("returns empty string if env variabled is disabled", () => {
+  it("returns empty string if env variable is disabled", () => {
     process.env.REACT_APP_CHANGESET_URL = "disabled";
-    const task = { parent: { changesetUrl: true, id: 1 } };
+    const task = { parent: { enabled: true, id: 1 } };
     expect(constructChangesetUrl(task)).toBe("");
   });
 
-  it("returns empty string changesetUrl is false", () => {
-    const task = { parent: { changesetUrl: false, id: 1 } };
+  it("returns empty string if challenge is not enabled", () => {
+    const task = { parent: { enabled: false, id: 1 } };
     expect(constructChangesetUrl(task)).toBe("");
   });
 
-  it("returns correct url if changesetUrl is true", () => {
-    const task = { parent: { changesetUrl: true, id: 1 } };
+  it("returns empty string if project is not enabled", () => {
+    const task = { parent: { parent: { enabled: false }, id: 1 } };
+    expect(constructChangesetUrl(task)).toBe("");
+  });
+
+  it("returns correct url if challenge and project are enabled", () => {
+    const task = { parent: { enabled: true, parent: { enabled: true }, id: 1 } };
     expect(constructChangesetUrl(task)).toBe(
       " http://localhost/browse/challenges/1"
     );
