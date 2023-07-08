82c31a7221c0869c7d8d06804a815142503cf81f
CollinBeczak <ctbeczak08@gmail.com>
2023-07-07 19:16:57 -0500

    fix merge reversion

--- src/services/Challenge/Challenge.js
+++ src/services/Challenge/Challenge.js
@@ -1071,18 +1071,6 @@ export const saveChallenge = function (
       return saveEndpoint
         .execute()
         .then((normalizedResults) => {
-          if (
-            challengeData.instruction === undefined ||
-            challengeData.instruction.length < 150 ||
-            challengeData.instruction.split(' ').length < 20 ||
-            challengeData.description === undefined ||
-            challengeData.description === '' ||
-            challengeData.name === undefined ||
-            challengeData.name.length <= 3
-          ) {
-            throw new Error();
-          }
-
           if (storeResponse) {
             dispatch(receiveChallenges(normalizedResults.entities));
           }
@@ -1093,21 +1081,6 @@ export const saveChallenge = function (
           );
         })
         .catch((serverError) => {
-          let errorMessage = 'challengeData.name.length';
-
-          if (challengeData.name === undefined || challengeData.name.length <= 3) {
-            errorMessage = AppErrors.challengeSaveFailure.saveNameFailure;
-          } else if (challengeData.description === undefined || challengeData.description === '') {
-            errorMessage = AppErrors.challengeSaveFailure.saveDescriptionFailure;
-          } else if (
-            challengeData.instruction === undefined ||
-            challengeData.instruction.length < 150 ||
-            challengeData.instruction.split(' ').length < 20
-          ) {
-            errorMessage = AppErrors.challengeSaveFailure.saveInstructionsFailure;
-          } else {
-            errorMessage = AppErrors.challengeSaveFailure.saveDetailsFailure;
-          }
           if (isSecurityError(serverError)) {
             dispatch(ensureUserLoggedIn()).then(() =>
               dispatch(addError(AppErrors.user.unauthorized))
