1514df7ffe52629f5d7397fe32660038831ad70b
Jimmy <jschwarz2030@gmail.com>
2023-08-16 11:20:28 -0700

    resolve some errors and warnings

--- src/components/Widgets/TaskMapWidget/RapidEditor/RapidEditor.js
+++ src/components/Widgets/TaskMapWidget/RapidEditor/RapidEditor.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import _get from 'lodash/get'
 import * as RapiD from '../../../../../node_modules/RapiD/dist/rapid.legacy.min.js';
 import '../../../../../node_modules/RapiD/dist/rapid.css';
@@ -7,11 +7,6 @@ import { UseRouter } from '../../../../hooks/UseRouter/UseRouter.js';
 import { constructRapidURI } from '../../../../services/Editor/Editor.js';
 import WithSearch from '../../../HOCs/WithSearch/WithSearch.js';
 import { DEFAULT_ZOOM } from '../../../../services/Challenge/ChallengeZoom/ChallengeZoom.js';
-
-const OSM_CLIENT_ID = process.env.REACT_APP_OSM_CLIENT_ID
-const OSM_CLIENT_SECRET = process.env.REACT_APP_OSM_CLIENT_SECRET
-const OSM_REDIRECT_URI = process.env.REACT_APP_URL
-const OSM_SERVER_URL = process.env.REACT_APP_OSM_SERVER
 
 const RapidEditor = ({
   comment,
@@ -27,11 +22,9 @@ const RapidEditor = ({
 }) => {
   const dispatch = useDispatch();
   const [customImageryIsSet, setCustomImageryIsSet] = useState(false);
-  // const session = useSelector((state) => state.auth.session);
   const [RapiDContext, setRapiDContext] = useState(null);
   const customSource =
     RapiDContext && RapiDContext.background() && RapiDContext.background().findSource('custom');
-  const windowInit = typeof window !== undefined;
   const router = UseRouter()
 
   useEffect(() => {
@@ -53,20 +46,8 @@ const RapidEditor = ({
   useEffect(() => {
     if (RapiDContext === null) {
       setRapiDContext(window.iD.coreContext())
-      //dispatch({ type: 'SET_RAPIDEDITOR', context: window.iD.coreContext() });
     }
   }, [RapiDContext, dispatch]);
-
-  // useEffect(() => {
-  //   if (windowInit) {
-  //     dispatch({ type: 'SET_VISIBILITY', isVisible: false });
-  //     if (RapiDContext === null) {
-  //       // we need to keep iD context on redux store because iD works better if
-  //       // the context is not restarted while running in the same browser session
-  //       dispatch({ type: 'SET_RAPIDEDITOR', context: window.iD.coreContext() });
-  //     }
-  //   }
-  // }, [windowInit, RapiDContext, dispatch]);
 
   useEffect(() => {
     if (mapBounds) {
@@ -122,12 +103,8 @@ const RapidEditor = ({
       let osm = RapiDContext.connection();
       const auth = {
         url: 'https://master.apis.dev.openstreetmap.org/',
-        client_id: 'RflKMjhJsuY5ktIYzuaVJ5JMOasjwRDtV0mq2OLPaxk',
-        client_secret: 'MtLDgWWn-KvdWCFbmVf54EVP2KRP_B8RWEzNbJwghys',
-        redirect_uri: OSM_REDIRECT_URI,
         access_token: token,
       };
-      console.log(auth)
       osm.switch(auth);
 
       const thereAreChanges = (changes) =>
