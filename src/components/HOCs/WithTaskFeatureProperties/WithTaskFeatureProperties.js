8ab9fbb54ef6d9d97b5c782830e4ca3ec767b605
Andrew Philbin <45773707+AndrewPhilbin@users.noreply.github.com>
2023-09-04 20:23:02 -0700

    Added jsdoc to WithTaskFeatureProperties HOC

--- src/components/HOCs/WithTaskFeatureProperties/WithTaskFeatureProperties.js
+++ src/components/HOCs/WithTaskFeatureProperties/WithTaskFeatureProperties.js
@@ -2,6 +2,13 @@ import React from 'react'
 import useMRProperties from '../../../hooks/UseMRProperties/UseMRProperties'
 import AsMappableTask from '../../../interactions/Task/AsMappableTask'
 
+/**
+ * WithTaskFeatureProperties provides task feature properties derived
+ * from the useMRProperties hook and the AsMappableTask interaction.
+ * Both property sets are combined and passed as props to wrapped components.
+ */
+
+
 const WithTaskFeatureProperties = (Component) => {
   return function(props) {
     const allFeatureProperties = AsMappableTask(props.task).allFeatureProperties()
