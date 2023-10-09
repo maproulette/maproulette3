cc50b35a7237103846870b32f4c43bcd6d8c1da8
Andrew Philbin <45773707+AndrewPhilbin@users.noreply.github.com>
2023-10-09 14:32:55 -0700

    removed unwanted import

--- src/components/TaskFilters/TaskStatusFilter.js
+++ src/components/TaskFilters/TaskStatusFilter.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 import { FormattedMessage } from 'react-intl'
 import FilterDropdown from './FilterDropdown'
 import TaskFilterIndicator from './TaskFilterIndicator'
