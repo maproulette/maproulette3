25c5767a1d522b75bb5d6c48cfb0fdd5872dcc7b
CollinBeczak <ctbeczak08@gmail.com>
2023-10-19 17:34:15 -0500

    add fail safe for widget layout prop

--- src/services/Challenge/Challenge.js
+++ src/services/Challenge/Challenge.js
@@ -963,7 +963,7 @@ export const fetchChallenges = function (
 
     // If there is local JSON content being transmitted as a string, parse
     // it into JSON first.
-    if (!challengeData.taskWidgetLayout.workspace && challengeData.taskWidgetLayout) {
+    if (!challengeData.taskWidgetLayout?.workspace && challengeData.taskWidgetLayout) {
       try {
         if (!(JSON.parse(challengeData.taskWidgetLayout).workspace.name === "taskCompletion")) {
           throw new Error("Widget layout for task completion flow with the wrong format was submitted, it was not included in the save.")
