3cad874f1a8b2e568308f87b78267bf84d8f1683
Jimmy <jschwarz2030@gmail.com>
2023-10-23 18:35:01 -0500

    remove unused vars

--- src/components/AdminPane/Manage/ManageChallenges/EditChallenge/BulkSchemas/DataSourceSchema.js
+++ src/components/AdminPane/Manage/ManageChallenges/EditChallenge/BulkSchemas/DataSourceSchema.js
@@ -1,12 +1,7 @@
-import _isEmpty from "lodash/isEmpty";
-import _omit from "lodash/omit";
 import messages from "../Messages";
 
 export const jsSchema = (
   intl,
-  user,
-  challengeData,
-  extraErrors,
 ) => {
   const schema = {
     $schema: "http://json-schema.org/draft-07/schema#",
@@ -25,10 +20,6 @@ export const jsSchema = (
 
 export const uiSchema = (
   intl,
-  user,
-  challengeData,
-  extraErrors,
-  options = {}
 ) => {
   return {
     dataOriginDate: {
