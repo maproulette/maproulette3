82f79a144de69f5cd9d84824847100f56430d340
jschwarz2030 <79289630+jschwarz2030@users.noreply.github.com>
2023-11-07 11:06:33 -0600

    support short url and short path (#2156)

--- src/utils/constructChangesetUrl.js
+++ src/utils/constructChangesetUrl.js
@@ -1,11 +1,15 @@
 export const constructChangesetUrl = (task) => {
   if (
     process.env.REACT_APP_CHANGESET_URL === "enabled" &&
+    task?.id &&
     task?.parent?.id &&
     task?.parent?.enabled &&
     task?.parent?.parent?.enabled
   ) {
-    return ` ${window.location.origin}/browse/challenges/${task.parent.id}`;
+    const rootUrl = process.env.REACT_APP_SHORT_URL || window.location.origin
+    const path = process.env.REACT_APP_SHORT_PATH === 'enabled' ? `/c/${task.parent.id}/t/${task.id}` : `/challenge/${task.parent.id}/task/${task.id}`
+
+    return ` ${rootUrl}${path}`;
   } else {
     return "";
   }
