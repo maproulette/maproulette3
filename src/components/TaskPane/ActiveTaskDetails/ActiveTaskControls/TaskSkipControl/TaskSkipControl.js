0260d7fd00864997025ecc2abe8a5c541b22b241
Collin Beczak <88843144+CollinBeczak@users.noreply.github.com>
2023-11-13 11:53:57 -0600

    make all keyboard shortcuts in option dropdown menu active (#2144)
    * make all keyboard shortcuts in option dropdown menu active
    
    * add no existent props handler
    
    * fix completion modal shortcuts to disable when rapid editor is open
    
    * add prevent default to remove reboard shortcut initially typing in text box
    
    * remove quickkeyhandler
    
    * combine duplicate code
    
    * fix editmode undefined
    
    * change function name

--- src/components/TaskPane/ActiveTaskDetails/ActiveTaskControls/TaskSkipControl/TaskSkipControl.js
+++ src/components/TaskPane/ActiveTaskDetails/ActiveTaskControls/TaskSkipControl/TaskSkipControl.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { FormattedMessage } from 'react-intl'
-import _pick from 'lodash/pick'
-import _isEmpty from 'lodash/isEmpty'
 import { TaskStatus }
        from '../../../../../services/Task/TaskStatus/TaskStatus'
 import Button from '../../../../Button/Button'
 import messages from './Messages'
-
-const shortcutGroup = 'taskCompletion'
 
 /**
  * TaskSkipControl displays a control for marking a task with a skipped status.
@@ -16,32 +12,6 @@ const shortcutGroup = 'taskCompletion'
  * @author [Neil Rotstan](https://github.com/nrotstan)
  */
 export default class TaskSkipControl extends Component {
-  completeTask = () => {
-    // Ignore if shortcut group is not active
-    if (_isEmpty(this.props.activeKeyboardShortcuts[shortcutGroup])) {
-      return
-    }
-
-    this.props.complete(TaskStatus.skipped)
-  }
-
-  handleKeyboardShortcuts = this.props.quickKeyHandler(
-    this.props.keyboardShortcutGroups.taskCompletion.skip.key,
-    () => this.completeTask()
-  )
-
-  componentDidMount() {
-    this.props.activateKeyboardShortcut(
-      shortcutGroup,
-      _pick(this.props.keyboardShortcutGroups.taskCompletion, 'skip'),
-      this.handleKeyboardShortcuts)
-  }
-
-  componentWillUnmount() {
-    this.props.deactivateKeyboardShortcut(shortcutGroup, 'skip',
-                                          this.handleKeyboardShortcuts)
-  }
-
   render() {
     if (this.props.asLink) {
       return (
--- src/components/TaskPane/ActiveTaskDetails/ActiveTaskControls/TaskTooHardControl/TaskTooHardControl.js
+++ src/components/TaskPane/ActiveTaskDetails/ActiveTaskControls/TaskTooHardControl/TaskTooHardControl.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { FormattedMessage } from 'react-intl'
-import _pick from 'lodash/pick'
-import _isEmpty from 'lodash/isEmpty'
 import { TaskStatus }
        from '../../../../../services/Task/TaskStatus/TaskStatus'
 import Button from '../../../../Button/Button'
 import messages from './Messages'
-
-const shortcutGroup = 'taskCompletion'
 
 /**
  * TaskTooHardControl displays a control for marking a task with a too-hard
@@ -17,31 +13,6 @@ const shortcutGroup = 'taskCompletion'
  * @author [Neil Rotstan](https://github.com/nrotstan)
  */
 export default class TaskTooHardControl extends Component {
-  completeTask = () => {
-    // Ignore if shortcut group is not active
-    if (_isEmpty(this.props.activeKeyboardShortcuts[shortcutGroup])) {
-      return
-    }
-
-    this.props.complete(TaskStatus.tooHard)
-  }
-
-  handleKeyboardShortcuts = this.props.quickKeyHandler(
-    this.props.keyboardShortcutGroups.taskCompletion.tooHard.key,
-    () => this.completeTask()
-  )
-
-  componentDidMount() {
-    this.props.activateKeyboardShortcut(
-      shortcutGroup,
-      _pick(this.props.keyboardShortcutGroups.taskCompletion, 'tooHard'),
-      this.handleKeyboardShortcuts)
-  }
-
-  componentWillUnmount() {
-    this.props.deactivateKeyboardShortcut(shortcutGroup, 'tooHard',
-                                          this.handleKeyboardShortcuts)
-  }
   render() {
     if (this.props.asLink) {
       return (
