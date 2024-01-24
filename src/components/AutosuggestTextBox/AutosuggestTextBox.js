914a46cd9dfd28b070a93c396f75fb8f6259d369
CollinBeczak <ctbeczak08@gmail.com>
2024-01-23 21:08:05 -0600

    fix checcked item appearing above all option on initial render

--- src/components/AutosuggestTextBox/AutosuggestTextBox.js
+++ src/components/AutosuggestTextBox/AutosuggestTextBox.js
@@ -143,11 +143,13 @@ export default class AutosuggestTextBox extends Component {
     const preferredResults = this.getPreferredResults()
     
     const reorderedSearchResults = searchResults.sort((a, b) => {
-      if(a.id === FILTER_SEARCH_ALL || a.name === this.props.inputValue) return -1 
-      if(b.id === FILTER_SEARCH_ALL || b.name === this.props.inputValue) return 1 
+      if (a.id === FILTER_SEARCH_ALL) return -1
+      if (b.id === FILTER_SEARCH_ALL) return 1
+      if (a.name === this.props.inputValue) return -1
+      if (b.name === this.props.inputValue) return 1
       return isChecked(b) - isChecked(a)
     })
-
+    
     if (!_isEmpty(preferredResults)) {
       let className = "mr-font-medium"
       items = items.concat(_map(reorderedSearchResults,
