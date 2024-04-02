f61a8405bcd2569401c1518885e967ad914affd0
CollinBeczak <ctbeczak08@gmail.com>
2024-04-02 12:57:18 -0500

    increase distance slightly

--- src/components/TaskClusterMap/TaskClusterMap.js
+++ src/components/TaskClusterMap/TaskClusterMap.js
@@ -580,7 +580,7 @@ export class TaskClusterMap extends Component {
     const overlapping = _filter(allMarkers, otherMarker => {
       if (otherMarker === marker) return false
       const dist = this.markerDistanceDegrees(marker, otherMarker)
-      return dist <= iconSizeDegrees/3
+      return dist <= iconSizeDegrees/2
     })
     return overlapping
   }
