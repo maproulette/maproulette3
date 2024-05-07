53248c62e9c552b3758ab1d441c7f333649aa75e
Andrew Philbin <45773707+AndrewPhilbin@users.noreply.github.com>
2024-05-07 13:27:04 -0700

    Added more specific content source paths to exclude non css being included in output

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   content: [
     './public/**/*.html',
-    './src/**/*.{js,jsx,ts,tsx}',
+    './src/components/**/*.{js,jsx,ts,tsx}',
+    './src/pages/**/*.{js,jsx,ts,tsx}'
   ],
   prefix: 'mr-',
   theme: {
