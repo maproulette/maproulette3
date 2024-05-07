caf6b741e0279d4f0af05e9fd55785cd74ce6f3e
Collin Beczak <88843144+CollinBeczak@users.noreply.github.com>
2024-05-07 14:35:53 -0500

    support user-friendly task identifier in search result markers (#2322)

--- src/components/ChallengePane/TaskChallengeMarkerContent.js
+++ src/components/ChallengePane/TaskChallengeMarkerContent.js
@@ -17,24 +17,31 @@ class TaskChallengeMarkerContent extends Component {
     }
 
     return (
-      <div className="marker-popup-content">
-        <h3>
-          <a onClick={() => this.props.history.push(
-            `/browse/challenges/${challengeId}`
-          )}>
-            {markerData.options.parentName || _get(this.props.task, 'parent.name')}
-          </a>
+      <div>
+        <h3 >
+          <div className="mr-text-left mr-text-grey">
+            <h4>{this.props.intl.formatMessage(messages.taskInfoLabel)}</h4>
+            <div className="mr-text-sm">
+      
+              {this.props.intl.formatMessage(messages.taskNameLabel)} {markerData.options.name || _get(this.props.task, 'name')}
+              <br />
+            {this.props.intl.formatMessage(messages.taskIdLabel)}   <a onClick={() => {
+              this.props.startChallengeWithTask(challengeId, false, markerData.options.taskId)
+            }}>{markerData.options.id || _get(this.props.task, 'id')}</a>
+              <br />
+              {this.props.intl.formatMessage(messages.challengeNameLabel)} {markerData.options.parentName || _get(this.props.task, 'parent.name')}
+              <br />
+              {this.props.intl.formatMessage(messages.challengeIdLabel)} <a onClick={() => this.props.history.push(`/browse/challenges/${challengeId}`)}>{markerData.options.parentId || _get(this.props.task, 'parent.id')}</a>
+            </div>
+          </div>
         </h3>
 
         <div className="marker-popup-content__links">
           <div>
             <a onClick={() => {
-              this.props.startChallengeWithTask(
-                challengeId,
-                false,
-                markerData.options.taskId)
+              this.props.startChallengeWithTask(challengeId, false, markerData.options.taskId)
             }}>
-              {this.props.intl.formatMessage(messages.startChallengeLabel)}
+              {this.props.intl.formatMessage(messages.startTaskLabel)} {markerData.options.id || _get(this.props.task, 'id')}
             </a>
           </div>
         </div>
