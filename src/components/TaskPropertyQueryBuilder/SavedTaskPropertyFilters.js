5115c6a63718ef5ac45b2a0506791ae034c595f1
Andrew Philbin <45773707+AndrewPhilbin@users.noreply.github.com>
2024-05-15 16:13:48 -0700

    added hoc and list components for saving and displaying task property rules

---
+++ src/components/TaskPropertyQueryBuilder/SavedTaskPropertyFilters.js
@@ -0,0 +1,18 @@
+function SavedTaskPropertyFilters() {
+  // Props: a clear form method
+  
+  // Need method to get saved task property filters. Maybe need to manage this upstream?
+
+  // Need method to add/update saved task property filters. Maybe need to manage this upstream?
+
+  // Method to delete task property filters. Maybe need to manage this upstream?
+  
+  // List of filters with a disclosure/collapsible for each showing details
+
+  // Need to filter out or deactivate filters that don't use keys relevant to current challenge
+
+  // Need to figure out presentation for complex filter values (tree of left/right operations, compound rules, etc, recursively generated into dom elements)
+  
+}
+
+export default SavedTaskPropertyFilters
