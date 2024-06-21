d3f806556e639856d8df7dfecef5976ae5fe436a
CollinBeczak <ctbeczak08@gmail.com>
2024-06-21 11:48:25 -0500

    remove unused import

--- src/components/TaskPane/PlaceDescription/PlaceDescription.js
+++ src/components/TaskPane/PlaceDescription/PlaceDescription.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 import classNames from 'classnames'
 import _get from 'lodash/get'
 import _isObject from 'lodash/isObject'
