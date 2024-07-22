2dbe50e93238dcf143ba73eae9e367bc95fbe22a
CollinBeczak <ctbeczak08@gmail.com>
2024-07-22 16:12:01 -0500

    remove tests for changing completion widget editing state

--- src/components/TaskPane/ActiveTaskDetails/ActiveTaskControls/TaskCompletionStep/TaskCompletionStep.test.js
+++ src/components/TaskPane/ActiveTaskDetails/ActiveTaskControls/TaskCompletionStep/TaskCompletionStep.test.js
@@ -1,7 +1,7 @@
 import "@testing-library/jest-dom";
 import * as React from "react";
 import { fireEvent, waitFor } from "@testing-library/react";
-import TaskCompletionStep, { ListMoreOptionsItems } from "./TaskCompletionStep";
+import TaskCompletionStep from "./TaskCompletionStep";
 
 describe("TaskCompletionStep", () => {
   it("renders task completion step 1 with required props", () => {
@@ -12,11 +12,15 @@ describe("TaskCompletionStep", () => {
         complete={() => null}
         allowedProgressions={new Map()}
         intl={{ formatMessage: () => null }}
+        keyboardShortcutGroups={{}}
+        activateKeyboardShortcutGroup={() => null}
+        deactivateKeyboardShortcutGroup={() => null}
       />
     );
     const text = getByText("Current Editor:");
     expect(text).toBeInTheDocument();
   });
+
 
   it("shows Edit button if allowedProgressions includes 1", () => {
     const allowedProgressions = new Map();
@@ -34,154 +38,7 @@ describe("TaskCompletionStep", () => {
         deactivateKeyboardShortcutGroup={() => null}
       />
     );
-    const text = getByText("Edit");
+    const text = getByText("Open Editor");
     expect(text).toBeInTheDocument();
-  });
-
-  it("shows an option to say I fixed it!", () => {
-    const allowedProgressions = new Map();
-    allowedProgressions.set(1);
-
-    const { getByText } = global.withProvider(
-      <ListMoreOptionsItems
-        task={{}}
-        pickEditor={() => null}
-        complete={() => null}
-        allowedProgressions={allowedProgressions}
-        intl={{ formatMessage: () => null }}
-        keyboardShortcutGroups={{ taskCompletion: { fixed: { key: 123 } } }}
-        activateKeyboardShortcutGroup={() => null}
-        deactivateKeyboardShortcutGroup={() => null}
-        activateKeyboardShortcut={() => null}
-        deactivateKeyboardShortcut={() => null}
-        needsRevised
-      />
-    );
-    const text = getByText("I fixed it!");
-    expect(text).toBeInTheDocument();
-  });
-
-  it("calls props.toggleDropdownVisible if list element is clicked - fixed it", async () => {
-    const toggleDropdownVisible = jest.fn()
-    const allowedProgressions = new Map();
-    allowedProgressions.set(1);
-
-    const { getByText, container } = global.withProvider(
-      <ListMoreOptionsItems
-        task={{}}
-        pickEditor={() => null}
-        complete={() => null}
-        allowedProgressions={allowedProgressions}
-        intl={{ formatMessage: () => null }}
-        keyboardShortcutGroups={{ taskCompletion: { fixed: { key: 123 } } }}
-        activateKeyboardShortcutGroup={() => null}
-        deactivateKeyboardShortcutGroup={() => null}
-        activateKeyboardShortcut={() => null}
-        deactivateKeyboardShortcut={() => null}
-        needsRevised
-        toggleDropdownVisible={toggleDropdownVisible}
-      />
-    );
-
-    const element = container.querySelector('li');
-
-    fireEvent.click(element);
-
-    await waitFor(() => {
-      expect(toggleDropdownVisible).toHaveBeenCalledTimes(1);
-    });
-  });
-
-  it("calls props.toggleDropdownVisible if list element is clicked - already fixed", async () => {
-    const toggleDropdownVisible = jest.fn()
-    const allowedProgressions = new Map();
-    allowedProgressions.set(2);
-
-    const { getByText, container } = global.withProvider(
-      <ListMoreOptionsItems
-        task={{}}
-        pickEditor={() => null}
-        complete={() => null}
-        allowedProgressions={allowedProgressions}
-        intl={{ formatMessage: () => null }}
-        keyboardShortcutGroups={{ taskCompletion: { falsePositive: { key: 123 } } }}
-        activateKeyboardShortcutGroup={() => null}
-        deactivateKeyboardShortcutGroup={() => null}
-        activateKeyboardShortcut={() => null}
-        deactivateKeyboardShortcut={() => null}
-        needsRevised
-        toggleDropdownVisible={toggleDropdownVisible}
-      />
-    );
-
-    const element = container.querySelector('li');
-
-    fireEvent.click(element);
-
-    await waitFor(() => {
-      expect(toggleDropdownVisible).toHaveBeenCalledTimes(1);
-    });
-  });
-
-  it("calls props.toggleDropdownVisible if list element is clicked - can't complete", async () => {
-    const toggleDropdownVisible = jest.fn()
-    const allowedProgressions = new Map();
-    allowedProgressions.set(6);
-
-    const { getByText, container } = global.withProvider(
-      <ListMoreOptionsItems
-        task={{}}
-        pickEditor={() => null}
-        complete={() => null}
-        allowedProgressions={allowedProgressions}
-        intl={{ formatMessage: () => null }}
-        keyboardShortcutGroups={{ taskCompletion: { tooHard: { key: 123 } } }}
-        activateKeyboardShortcutGroup={() => null}
-        deactivateKeyboardShortcutGroup={() => null}
-        activateKeyboardShortcut={() => null}
-        deactivateKeyboardShortcut={() => null}
-        needsRevised
-        toggleDropdownVisible={toggleDropdownVisible}
-      />
-    );
-
-    const element = container.querySelector('li');
-
-    fireEvent.click(element);
-
-    await waitFor(() => {
-      expect(toggleDropdownVisible).toHaveBeenCalledTimes(1);
-    });
-  });
-
-  it("calls props.toggleDropdownVisible if list element is clicked - already fixed", async () => {
-    const toggleDropdownVisible = jest.fn()
-    const allowedProgressions = new Map();
-    allowedProgressions.set(5);
-
-    const { getByText, container } = global.withProvider(
-      <ListMoreOptionsItems
-        task={{}}
-        pickEditor={() => null}
-        complete={() => null}
-        allowedProgressions={allowedProgressions}
-        intl={{ formatMessage: () => null }}
-        keyboardShortcutGroups={{ taskCompletion: { alreadyFixed: { key: 123 } } }}
-        activateKeyboardShortcutGroup={() => null}
-        deactivateKeyboardShortcutGroup={() => null}
-        activateKeyboardShortcut={() => null}
-        deactivateKeyboardShortcut={() => null}
-        needsRevised
-        toggleDropdownVisible={toggleDropdownVisible}
-      />
-    );
-
-    const element = container.querySelector('li');
-
-    fireEvent.click(element);
-
-    await waitFor(() => {
-      expect(toggleDropdownVisible).toHaveBeenCalledTimes(1);
-    });
   });
 });
