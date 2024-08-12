8520ba6569d47b809c22b21d8faefe1c55e2c905
Collin Beczak <88843144+CollinBeczak@users.noreply.github.com>
2024-08-12 09:21:14 -0500

    add your locked tasks widget (#2389)
    * add your locked tasks widget

---
+++ src/components/LockedTasks/Messages.js
@@ -0,0 +1,46 @@
+import { defineMessages } from 'react-intl'
+
+/**
+ * Internationalized messages for use with SavedChallenges.
+ */
+export default defineMessages({
+  header: {
+    id: "UserProfile.lockedTasks.header",
+    defaultMessage: "Your Locked Tasks",
+  },
+
+  noLockedTasks: {
+    id: "SavedChallenges.widget.noTasks",
+    defaultMessage: "You have no locked tasks",
+  },
+
+  taskLabel: {
+    id: "Admin.Task.fields.name.label",
+    defaultMessage: "Task:",
+  },
+
+  challengeLabel: {
+    id: 'TaskConfirmationModal.challenge.label',
+    defaultMessage: "Challenge:",
+  },
+
+  description: {
+    id: "SavedChallenges.widget.description",
+    defaultMessage: "Tasks locked for more than an hour will be automatically unlocked within the next hour or might already be unlocked. ",
+  },
+
+  checkList: {
+    id: "SavedChallenges.widget.checkList.label",
+    defaultMessage: "Refresh list to check.",
+  },
+
+  unlockLabel: {
+    id: "Task.pane.controls.unlock.label",
+    defaultMessage: "You have no locked tasks",
+  },
+
+  taskLockedLabel: {
+    id: "ReviewTaskPane.indicators.locked.label",
+    defaultMessage: "Task locked",
+  },
+})
--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -23,6 +23,7 @@ export const defaultWorkspaceSetup = function() {
     widgets: [
       widgetDescriptor('FeaturedChallengesWidget'),
       widgetDescriptor('SavedChallengesWidget'),
+      widgetDescriptor('LockedTasksWidget'),
       widgetDescriptor('TopUserChallengesWidget'),
       widgetDescriptor('UserActivityTimelineWidget'),
       widgetDescriptor('PopularChallengesWidget'),
