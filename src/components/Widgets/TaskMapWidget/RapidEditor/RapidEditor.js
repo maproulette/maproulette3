8eabe069e506546f7f61ddade99e5d71d5cfb8bd
Jake Low <hello@jakelow.com>
2024-09-03 13:59:20 -0700

    Pass token in hash instead of query params for security

--- src/components/Widgets/TaskMapWidget/RapidEditor/RapidEditor.js
+++ src/components/Widgets/TaskMapWidget/RapidEditor/RapidEditor.js
@@ -51,7 +51,7 @@ const RapidEditor = ({ token, task, mapBounds, comment }) => {
     <iframe
       id="rapid-container-root"
       style={{ width: '100%', height: '100%' }}
-      src={`/rapid-editor.html?token=${token}#${initialHash}`}
+      src={`/rapid-editor.html${initialHash}&token=${token}`}
       onLoad={async (event) => {
         let iframe = event.target;
         iframe.contentWindow.token = token;
