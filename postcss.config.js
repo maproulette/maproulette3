4ab614e184718d6a2a8a8db125cf776e90a51f7c
Jake Low <hello@jakelow.com>
2024-10-02 14:57:39 -0700

    Fix relative image URLs in backgrounds.css

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,12 @@
 export default {
   plugins: {
     'postcss-import': {},
-    'postcss-simple-vars': {},
+    'postcss-simple-vars': {
+      // leave references to undefined variables like $foo untransformed,
+      // instead of erroring. needed because Vite transforms url() paths
+      // into a base64-encoding that may contain '$' characters.
+      silent: true
+    },
     'tailwindcss/nesting': {},
     'tailwindcss': { config: './src/tailwind.config.js'},
     'autoprefixer': {},
