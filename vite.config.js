8ea9b2d0c0974f9260c0ebbc632a77ad0b0733e3
Jake Low <hello@jakelow.com>
2024-10-02 14:54:39 -0700

    Add source maps to builds

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
+  build: {
+    sourcemap: true,
+  },
   plugins: [react()]
 })
 
