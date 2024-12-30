7d64b6f7aaafa0b774652d13dfa178a851354058
Collin Beczak <88843144+CollinBeczak@users.noreply.github.com>
2024-12-30 15:32:34 -0600

    make tasks in task properties widget collapsible (#2502)
    * make tasks in task properties widget collapsible
    
    * simplify dropdown logic and improve styles

--- src/components/Widgets/TaskPropertiesWidget/TaskPropertiesWidget.jsx
+++ src/components/Widgets/TaskPropertiesWidget/TaskPropertiesWidget.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react'
+import React, { useState } from 'react'
 import { FormattedMessage } from 'react-intl'
 import _get from 'lodash/get'
 import _map from 'lodash/map'
@@ -8,6 +8,7 @@ import PropertyList from '../../EnhancedMap/PropertyList/PropertyList'
 import AsMappableTask from '../../../interactions/Task/AsMappableTask'
 import QuickWidget from '../../QuickWidget/QuickWidget'
 import messages from './Messages'
+import './TaskPropertiesWidget.scss'
 
 const descriptor = {
   widgetKey: 'TaskPropertiesWidget',
@@ -19,45 +20,59 @@ const descriptor = {
   defaultHeight: 6,
 }
 
-export default class TaskPropertiesWidget extends Component {
-  render() {
-    const taskList = _get(this.props.taskBundle, 'tasks') || [this.props.task]
-    const propertyLists = _map(taskList, (task) => {
-      const featurePropertiesList = AsMappableTask(task).osmFeatureProperties(this.props.osmElements)
+const TaskPropertiesWidget = (props) => {
+  const taskList = _get(props.taskBundle, 'tasks') || [props.task];
+  const [collapsed, setCollapsed] = useState();
 
-      return (
-        <div key={task.id} className="mr-mb-6">
-          <div className="mr-text-yellow">
-            <FormattedMessage {...messages.taskLabel} values={{taskId: task.id}}/>
-          </div>
-          {
-            featurePropertiesList.map((feature, index) => {
-              return (
-                <div key={index}>
-                  <div className="mr-text-yellow mr-ml-2">
-                    {feature.properties?.id || feature.geometry?.type}
-                  </div>
-                  <PropertyList featureProperties={feature.properties} hideHeader
-                    lightMode={false} {...this.props} />
-                </div>
-              )
-            })
-          }
-        </div>
-      )
-    })
+  const toggleCollapsed = () => {
+    setCollapsed(!collapsed);
+  }
 
+  const propertyLists = _map(taskList, (task) => {
+    const featurePropertiesList = AsMappableTask(task).osmFeatureProperties(props.osmElements);
 
     return (
-      <QuickWidget
-        {...this.props}
-        className="task-properties-widget"
-        widgetTitle={<FormattedMessage {...messages.title} />}
-      >
-        {propertyLists}
-      </QuickWidget>
+      <div key={task.id} className="mr-mb-4 border-b mr-border-gray-300 pb-2">
+        {
+          featurePropertiesList.map((feature, index) => {
+            const featureIndex = `${task.id}-${index}`;
+            return (
+              <details key={featureIndex} open={!collapsed} className="mr-pb-2 mr-border-b mr-bg-gray-100">
+                <summary className="mr-text-yellow mr-cursor-pointer mr-p-1 mr-rounded mr-flex mr-justify-between mr-items-center">
+                  <div className="mr-flex mr-items-center">
+                    <div className="mr-text-yellow mr-font-bold mr-text-lg">
+                      <FormattedMessage {...messages.taskLabel} values={{taskId: task.id}}/>
+                    </div>
+                    <span className="mr-text-grey-lighter mr-mx-2 mr-mt-1">
+                      {feature.properties?.id || feature.geometry?.type}
+                    </span>
+                  </div>
+                </summary>
+                <PropertyList featureProperties={feature.properties} hideHeader lightMode={false} {...props} />
+              </details>
+            )
+          })
+        }
+      </div>
     )
-  }
+  })
+
+  return (
+    <QuickWidget
+      {...props}
+      className="task-properties-widget"
+      widgetTitle={<FormattedMessage {...messages.title} />}
+      rightHeaderControls={
+        <div onClick={toggleCollapsed} className="mr-cursor-pointer mr-button mr-button--small mr-text-sm mr-ml-4">
+          {collapsed ? <FormattedMessage {...messages.expandAll} /> : <FormattedMessage {...messages.collapseAll} />}
+        </div>
+      }
+    >
+      {propertyLists}
+    </QuickWidget>
+  )
 }
 
 registerWidgetType(TaskPropertiesWidget, descriptor)
+
+export default TaskPropertiesWidget;
