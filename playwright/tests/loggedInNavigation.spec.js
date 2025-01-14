583d073aeb03533f2caef3f771f677622eee4e3c
CollinBeczak <ctbeczak08@gmail.com>
2025-01-13 21:47:36 -0600

    remove describe from tests

--- playwright/tests/loggedInNavigation.spec.js
+++ playwright/tests/loggedInNavigation.spec.js
@@ -1,15 +1,13 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('Logged in navigation', () => {
-  test.use({ storageState: './state.json' });
-  test.beforeEach(async ({ page }) => {
-    await page.goto(process.env.REACT_APP_PLAYWRIGHT_URL);
-    await page.waitForLoadState('networkidle');
-    await page.getByRole('banner').locator('a').filter({ hasText: 'Sign in' }).click();
-  });
+test.use({ storageState: './state.json' });
+test.beforeEach(async ({ page }) => {
+  await page.goto(process.env.REACT_APP_PLAYWRIGHT_URL);
+  await page.waitForLoadState('networkidle');
+  await page.getByRole('banner').locator('a').filter({ hasText: 'Sign in' }).click();
+});
 
-  test('should navigate to Find Challenges', async ({ page }) => {
-    await page.getByRole('navigation').getByRole('link', { name: 'Find Challenges' }).click();
-    await expect(page.getByRole('heading', { name: 'Challenges' }).locator('span')).toBeVisible();
-  });
+test('should navigate to Find Challenges', async ({ page }) => {
+  await page.getByRole('navigation').getByRole('link', { name: 'Find Challenges' }).click();
+  await expect(page.getByRole('heading', { name: 'Challenges' }).locator('span')).toBeVisible();
 });
