d8ce0097899a7670ebc9a9dfc1a15422352f5cda
Jake Low <hello@jakelow.com>
2025-03-11 17:24:13 -0700

    Port super-admin MetricsTable to React Table v7

--- src/components/SuperAdmin/MetricsTable.jsx
+++ src/components/SuperAdmin/MetricsTable.jsx
@@ -1,19 +1,17 @@
-import { useState } from "react";
+import { useMemo } from "react";
 import { injectIntl } from "react-intl";
-import ReactTable from "react-table-6";
+import { useTable, usePagination, useSortBy } from "react-table";
 import BusySpinner from "../BusySpinner/BusySpinner";
 import WithSortedChallenges from "../HOCs/WithSortedChallenges/WithSortedChallenges";
-import { setChallengeTab, setProjectTab, setUserTab } from "./MetricsData";
+import { CHALLENGE_COLUMNS, PROJECT_COLUMNS, USER_COLUMNS } from "./MetricsData";
 import WithMetricsSearchResults from "./WithMetricsSearchResults";
 import WithSortedProjects from "./WithSortedProjects";
 import WithSortedUsers from "./WithSortedUsers";
-
+import PaginationControl from "../PaginationControl/PaginationControl";
 const MetricsTable = (props) => {
-  const [userChanges, setUserChanges] = useState({});
-  let data;
-  const constructHeader = () => {
+  const data = useMemo(() => {
     if (props.currentTab === "challenges") {
-      data = props.challenges.map((c) => ({
+      return props.challenges.map((c) => ({
         id: c.id,
         name: c.name,
         parent: c.parent,
@@ -26,10 +24,8 @@ const MetricsTable = (props) => {
         dataOriginDate: c.dataOriginDate,
         lastTaskRefresh: c.lastTaskRefresh,
       }));
-
-      return setChallengeTab(props);
     } else if (props.currentTab === "projects") {
-      data = props.projects.map((p) => ({
+      return props.projects.map((p) => ({
         id: p.id,
         displayName: p.displayName,
         owner: p.owner,
@@ -39,10 +35,8 @@ const MetricsTable = (props) => {
         created: p.created,
         modified: p.modified,
       }));
-
-      return setProjectTab(props);
     } else if (props.currentTab === "users") {
-      data = props.users.map((u) => ({
+      return props.users.map((u) => ({
         id: u.id,
         displayName: u.osmProfile.displayName,
         score: u.score,
@@ -50,17 +44,94 @@ const MetricsTable = (props) => {
         modified: u.modified,
         superUser: Boolean(u.grants?.find((grant) => grant.role === -1)),
       }));
-
-      return setUserTab(userChanges, setUserChanges);
+    } else { 
+      return [];
     }
-  };
+  }, [props.currentTab, props.challenges, props.projects, props.users]);
 
-  return !props.isloadingCompleted ? (
-    <div className="admin mr-flex mr-justify-center mr-py-8 mr-w-full mr-bg-blue">
-      <BusySpinner />
-    </div>
-  ) : (
-    <ReactTable columns={constructHeader()} data={data} defaultPageSize={50} />
+  const columns = useMemo(() => {
+    if (props.currentTab === "challenges") {
+      return CHALLENGE_COLUMNS;
+    } else if (props.currentTab === "projects") {
+      return PROJECT_COLUMNS;
+    } else if (props.currentTab === "users") {
+      return USER_COLUMNS;
+    }
+    return [];
+  }, [props.currentTab]);
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page,
+    prepareRow,
+    state: { pageIndex, pageSize },
+    gotoPage,
+    setPageSize,
+  } = useTable(
+    {
+      columns,
+      data,
+      initialState: { pageIndex: 0, pageSize: 50 },
+      pageCount: Math.ceil(data.length / 50),
+    },
+    useSortBy,
+    usePagination
+  );
+
+  if (!props.isloadingCompleted) {
+    return (
+      <div className="admin mr-flex mr-justify-center mr-py-8 mr-w-full mr-bg-blue">
+        <BusySpinner />
+      </div>
+    );
+  }
+
+  return (
+    <section>
+      <table className="mr-w-full mr-text-white mr-links-green-lighter" {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th
+                  {...column.getHeaderProps(column.getSortByToggleProps())}
+                  className="mr-text-left mr-px-2 mr-whitespace-nowrap"
+                >
+                  {column.render("Header")}
+                  {column.isSorted ? (column.isSortedDesc ? " ▼" : " ▲") : ""}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+
+        <tbody {...getTableBodyProps()}>
+          {page.map((row) => {
+            prepareRow(row);
+            return (
+              <tr className="mr-border-y mr-border-white-10" {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td className="mr-px-2 mr-whitespace-nowrap" {...cell.getCellProps()}>
+                      {cell.render("Cell")}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <PaginationControl
+          currentPage={pageIndex}
+          totalPages={Math.ceil(data.length / pageSize)}
+          pageSize={pageSize}
+          gotoPage={gotoPage}
+          setPageSize={setPageSize}
+        />
+    </section>
   );
 };
 
