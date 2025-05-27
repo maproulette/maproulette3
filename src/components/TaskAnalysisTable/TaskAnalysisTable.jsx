c38d36a91deabf93d129dff4746c8716f3490bfc
Jake Low <hello@jakelow.com>
2025-05-27 10:59:44 -0700

    Fix formatting error in TaskAnalysisTable.jsx

--- src/components/TaskAnalysisTable/TaskAnalysisTable.jsx
+++ src/components/TaskAnalysisTable/TaskAnalysisTable.jsx
@@ -71,14 +71,7 @@ const ALL_COLUMNS = Object.assign(
     : null,
 );
 
-const DEFAULT_COLUMNS = [
-  "featureId",
-  "id",
-  "status",
-  "priority",
-  "controls",
-  "comments",
-];
+const DEFAULT_COLUMNS = ["featureId", "id", "status", "priority", "controls", "comments"];
 
 /**
  * TaskAnalysisTable renders a table of tasks using react-table.  Rendering is
