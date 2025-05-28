c1a22c49959626be3f58ebdc44dd740782df842a
John Lewin <lewin76@gmail.com>
2025-05-27 20:45:34 -0700

    Add tests to validate revised regular expression behavior

--- src/hooks/UsePropertyReplacement/UsePropertyReplacement.test.js
+++ src/hooks/UsePropertyReplacement/UsePropertyReplacement.test.js
@@ -21,6 +21,41 @@ describe("usePropertyReplacement additional tests", () => {
     expect(result.current).toBe("[link](https://example.com/deep%20value)");
   });
 
+  it("performs basic url encoded replacement in markdown link", () => {
+    const content = "[link](https://example.com/{{name}})";
+    const expected = "[link](https://example.com/Test%20Name)";
+    const { result } = renderHook(() => usePropertyReplacement(content, properties));
+    expect(result.current).toBe(expected);
+  });
+
+  it("ignores whitespace between link text and URL in markdown link", () => {
+    const content = "[link]    (https://example.com/{{name}})";
+    const expected = "[link](https://example.com/Test%20Name)";
+    const { result } = renderHook(() => usePropertyReplacement(content, properties));
+    expect(result.current).toBe(expected);
+  });
+
+  it("falls through to preexisting replacement behavior for invalid link format", () => {
+    const content = "[link] hello there (https://example.com/{{name}})";
+    const expected = "[link] hello there (https://example.com/Test Name)";
+    const { result } = renderHook(() => usePropertyReplacement(content, properties));
+    expect(result.current).toBe(expected);
+  });
+
+  it("does not url encode standalone replacements outside markdown links", () => {
+    const content = "[link](https://example.com/) blab blah {{name}} blah [another link](https://example.com/)";
+    const expected = "[link](https://example.com/) blab blah Test Name blah [another link](https://example.com/)";
+    const { result } = renderHook(() => usePropertyReplacement(content, properties));
+    expect(result.current).toBe(expected);
+  });
+
+  it("does not url encode standalone replacements when no markdown links are present", () => {
+    const content = "hello there (foo {{name}})";
+    const expected = "hello there (foo Test Name)";
+    const { result } = renderHook(() => usePropertyReplacement(content, properties));
+    expect(result.current).toBe(expected);
+  });
+
   it("only url encodes replacements in markdown links", () => {
     const content = "{{nested.deep.key}} [link](https://example.com/{{nested.deep.key}})";
     const { result } = renderHook(() => usePropertyReplacement(content, properties, true, false));
