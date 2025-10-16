7f5d167a7b78206432ef3edb6f9d0e695a47ed7e
Collin Beczak <ctbeczak08@gmail.com>
2025-10-16 16:02:17 -0300

    run formatting script

--- src/utils/featureTypes.tsx
+++ src/utils/featureTypes.tsx
@@ -1,27 +1,32 @@
-import type { LineString, Point, Polygon } from "@/types/Task";
+import type { LineString, Point, Polygon } from '@/types/Task'
 
 export const isPoint = (geometry: { type: string; coordinates: unknown }): geometry is Point => {
-    return (
-      geometry.type === 'Point' &&
-      Array.isArray(geometry.coordinates) &&
-      geometry.coordinates.length === 2
-    )
-  }
-  
-  export const isLineString = (geometry: { type: string; coordinates: unknown }): geometry is LineString => {
-    return (
-      geometry.type === 'LineString' &&
-      Array.isArray(geometry.coordinates) &&
-      Array.isArray(geometry.coordinates[0])
-    )
-  }
-  
-  export const isPolygon = (geometry: { type: string; coordinates: unknown }): geometry is Polygon => {
-    return (
-      geometry.type === 'Polygon' &&
-      Array.isArray(geometry.coordinates) &&
-      Array.isArray(geometry.coordinates[0]) &&
-      Array.isArray(geometry.coordinates[0][0])
-    )
-  }
-  
+  return (
+    geometry.type === 'Point' &&
+    Array.isArray(geometry.coordinates) &&
+    geometry.coordinates.length === 2
+  )
+}
+
+export const isLineString = (geometry: {
+  type: string
+  coordinates: unknown
+}): geometry is LineString => {
+  return (
+    geometry.type === 'LineString' &&
+    Array.isArray(geometry.coordinates) &&
+    Array.isArray(geometry.coordinates[0])
+  )
+}
+
+export const isPolygon = (geometry: {
+  type: string
+  coordinates: unknown
+}): geometry is Polygon => {
+  return (
+    geometry.type === 'Polygon' &&
+    Array.isArray(geometry.coordinates) &&
+    Array.isArray(geometry.coordinates[0]) &&
+    Array.isArray(geometry.coordinates[0][0])
+  )
+}
