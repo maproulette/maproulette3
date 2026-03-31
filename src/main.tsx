151960b377ba170c7a313b577ef23d0435d1771c
Collin Beczak <ctbeczak08@gmail.com>
2026-03-31 14:42:54 -0500

    remove web vitals tracking

--- src/main.tsx
+++ src/main.tsx
@@ -77,7 +77,3 @@ if (rootElement && !rootElement.innerHTML) {
   )
 }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals()
