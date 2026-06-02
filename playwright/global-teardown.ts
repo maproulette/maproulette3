c267f53cc0a6d6a925ec4b4eb07e863803283cd5
Jake Low <hello@jakelow.com>
2026-06-02 15:23:27 -0700

    Remove existing Playwright test suite and CI scripts

--- playwright/global-teardown.ts
+++
@@ -1,7 +0,0 @@
-import { stopBackend } from './utils/backend.js'
-import { stopDatabase } from './utils/database.js'
-
-export default async function globalTeardown() {
-  stopBackend()
-  stopDatabase()
-}
