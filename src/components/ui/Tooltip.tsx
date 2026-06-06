76019d2ad0a6dc9ab447150756df91e939844885
Jake Low <hello@jakelow.com>
2026-06-05 21:31:05 -0700

    Flip tooltip light and dark styles, and fix arrow color

--- src/components/ui/Tooltip.tsx
+++ src/components/ui/Tooltip.tsx
@@ -37,13 +37,13 @@ export const TooltipContent = ({
       data-slot="tooltip-content"
       sideOffset={sideOffset}
       className={cn(
-        'fade-in-0 zoom-in-95 data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 w-fit origin-(--radix-tooltip-content-transform-origin) animate-in text-balance rounded-lg bg-zinc-950 px-3 py-1.5 text-white text-xs data-[state=closed]:animate-out dark:bg-slate-50 dark:text-zinc-950',
+        'fade-in-0 zoom-in-95 data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 w-fit origin-(--radix-tooltip-content-transform-origin) animate-in text-balance rounded-lg bg-slate-50 px-3 py-1.5 text-xs text-zinc-950 data-[state=closed]:animate-out dark:bg-zinc-950 dark:text-white',
         className
       )}
       {...props}
     >
       {children}
-      <TooltipPrimitive.Arrow className="z-50 size-2.5 translate-y-[calc(-50%_-_2px)] rotate-45 rounded-[2px] bg-zinc-950 fill-foreground dark:bg-slate-50" />
+      <TooltipPrimitive.Arrow className="z-50 size-2.5 translate-y-[calc(-50%_-_2px)] rotate-45 rounded-[2px] bg-slate-50 fill-slate-50 dark:bg-zinc-950 dark:fill-zinc-950" />
     </TooltipPrimitive.Content>
   </TooltipPrimitive.Portal>
 )
