ac959768656727fb569aac90df1aee3de95d19cc
Jake Low <hello@jakelow.com>
2026-06-05 20:17:28 -0700

    Add border to dropdown menu to match select and popover

--- src/components/ui/DropdownMenu.tsx
+++ src/components/ui/DropdownMenu.tsx
@@ -32,7 +32,7 @@ export const DropdownMenuContent = ({
       data-slot="dropdown-menu-content"
       sideOffset={sideOffset}
       className={cn(
-        'data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 max-h-(--radix-dropdown-menu-content-available-height) min-w-[8rem] origin-(--radix-dropdown-menu-content-transform-origin) overflow-y-auto overflow-x-hidden rounded-lg bg-white p-1 shadow-xl data-[state=closed]:animate-out data-[state=open]:animate-in dark:bg-slate-900 dark:text-zinc-50',
+        'data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 max-h-(--radix-dropdown-menu-content-available-height) min-w-[8rem] origin-(--radix-dropdown-menu-content-transform-origin) overflow-y-auto overflow-x-hidden rounded-lg border border-zinc-200 bg-white p-1 shadow-xl data-[state=closed]:animate-out data-[state=open]:animate-in dark:border-slate-800 dark:bg-slate-900 dark:text-zinc-50',
         className
       )}
       {...props}
@@ -188,7 +188,7 @@ export const DropdownMenuSubContent = ({
   <DropdownMenuPrimitive.SubContent
     data-slot="dropdown-menu-sub-content"
     className={cn(
-      'data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 min-w-[8rem] origin-(--radix-dropdown-menu-content-transform-origin) overflow-hidden rounded-lg bg-white p-1 shadow-xl data-[state=closed]:animate-out data-[state=open]:animate-in dark:bg-slate-900 dark:text-zinc-50',
+      'data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 min-w-[8rem] origin-(--radix-dropdown-menu-content-transform-origin) overflow-hidden rounded-lg border border-zinc-200 bg-white p-1 shadow-xl data-[state=closed]:animate-out data-[state=open]:animate-in dark:border-slate-800 dark:bg-slate-900 dark:text-zinc-50',
       className
     )}
     {...props}
